d848a2333914d31c5713d9857701e10222cd66e6
5yex <83497667+5yex@users.noreply.github.com>
2022-03-30 18:21:39 +0200

    autoCommit 20220330.182139

---
+++ ScriptWindows.bat
@@ -0,0 +1,13 @@
+@echo off 
+
+set directorio=%cd%
+set segundos=10
+
+cd %directorio%
+:loop
+git pull
+git add --all
+git commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
+git push
+timeout /t 10 /nobreak
+goto :loop
