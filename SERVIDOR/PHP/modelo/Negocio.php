1441c7748ff17d88d203a4eee153be21f80a54c3
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-04-08 18:22:28 +0200

    autoCommit 20220408.182228

--- SERVIDOR/PHP/modelo/Negocio.php
+++ SERVIDOR/PHP/modelo/Negocio.php
@@ -1,15 +1,70 @@
 <?php
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
- */
+require_once '../controlador/conexion.php';
 
 /**
  * Description of Negocio
  *
  * @author Jesus
  */
-class Negocio {
-    //put your code here
+class Negocio extends Conexion{
+    private $id_negocio;
+    private $id_ciudad;
+    private $id_categoria;
+    private $id_mercader;
+    private $nombre;
+    private $descripcion;
+    
+    public function __construct() {
+        
+    }
+    
+    public function getId_negocio() {
+        return $this->id_negocio;
+    }
+
+    public function getId_ciudad() {
+        return $this->id_ciudad;
+    }
+
+    public function getId_categoria() {
+        return $this->id_categoria;
+    }
+
+    public function getId_mercader() {
+        return $this->id_mercader;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getDescripcion() {
+        return $this->descripcion;
+    }
+
+    public function setId_negocio($id_negocio): void {
+        $this->id_negocio = $id_negocio;
+    }
+
+    public function setId_ciudad($id_ciudad): void {
+        $this->id_ciudad = $id_ciudad;
+    }
+
+    public function setId_categoria($id_categoria): void {
+        $this->id_categoria = $id_categoria;
+    }
+
+    public function setId_mercader($id_mercader): void {
+        $this->id_mercader = $id_mercader;
+    }
+
+    public function setNombre($nombre): void {
+        $this->nombre = $nombre;
+    }
+
+    public function setDescripcion($descripcion): void {
+        $this->descripcion = $descripcion;
+    }
+
+
 }
