71783dbace77719fecd619698215ff371159fa80
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-08 17:56:52 +0200

    autoCommit 20220408.175652

--- SERVIDOR/PHP/modelo/usuario.php
+++ SERVIDOR/PHP/modelo/usuario.php
@@ -1,15 +1,10 @@
 <?php
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
- */
-
+ require_once '../controlador/conexion.php';
 /**
  * Description of usuario
  *
  * @author jmcbg
  */
 class usuario {
-    //put your code here
+   
 }
