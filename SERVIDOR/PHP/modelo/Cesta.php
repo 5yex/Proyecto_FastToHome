cd2c19e39951ea77838b9220b7f02d1d4845c216
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-04-08 18:26:56 +0200

    autoCommit 20220408.182655

--- SERVIDOR/PHP/modelo/Cesta.php
+++ SERVIDOR/PHP/modelo/Cesta.php
@@ -7,5 +7,45 @@ require_once '../controlador/conexion.php';
  * @author Jesus
  */
 class Cesta extends Conexion{
-    //put your code here
+    private $id_cesta;
+    private $cantidad;
+    private $id_pedido;
+    private $id_producto;
+    
+    public function __construct() {
+        
+    }
+    
+    public function getId_cesta() {
+        return $this->id_cesta;
+    }
+
+    public function getCantidad() {
+        return $this->cantidad;
+    }
+
+    public function getId_pedido() {
+        return $this->id_pedido;
+    }
+
+    public function getId_producto() {
+        return $this->id_producto;
+    }
+
+    public function setId_cesta($id_cesta): void {
+        $this->id_cesta = $id_cesta;
+    }
+
+    public function setCantidad($cantidad): void {
+        $this->cantidad = $cantidad;
+    }
+
+    public function setId_pedido($id_pedido): void {
+        $this->id_pedido = $id_pedido;
+    }
+
+    public function setId_producto($id_producto): void {
+        $this->id_producto = $id_producto;
+    }
+
 }
