81a8599760022a84d90ceb352b98fbead3888817
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-08 18:46:06 +0200

    autoCommit 20220408.184606

--- SERVIDOR/PHP/webService/listarUsuario.php
+++ SERVIDOR/PHP/webService/listarUsuario.php
@@ -1,7 +1,28 @@
 <?php
+
+// $sentencia->bindParam(":nom", $this->getNombre());
+//        $sentencia->bindParam(":dni", $this->getDni());
+//        $sentencia->bindParam(":email", $this->getEmail());
+//        $sentencia->bindParam(":tlf", $this->getTlf());
+//        $sentencia->bindParam(":rol", $this->getRol());
+//        $sentencia->bindParam(":dir", $this->getId_direccion());
+//        $sentencia->bindParam(":pass", $this->getPassword());
+
+
     require_once '../modelo/usuario.php';
-    $objUsuario = new ;
-    //$codigo= $_POST["codigo"];
+    $objUsuario = new usuario();
+    $dni= $_POST["dni"];
+    $email= $_POST["email"];
+    $dni= $_POST["dni"];
+   $tlf= $_POST["tlf"];
+   $rol= $_POST["rol"];
+   $direccion= $_POST["dir"];
+   $password= $_POST["pass"];
+   
+   
+   
+   
+    
     $objUsuario->setCodigo($codigo);
     $resultado = $objUsuario->listarCodigo();
     $respuesta = array(
