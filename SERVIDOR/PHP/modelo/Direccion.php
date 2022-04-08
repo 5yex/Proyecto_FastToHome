62440ce215b6ab9ef54e58e8b36fd7c35208231f
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-04-08 18:04:55 +0200

    autoCommit 20220408.180455

--- SERVIDOR/PHP/modelo/Direccion.php
+++ SERVIDOR/PHP/modelo/Direccion.php
@@ -10,14 +10,82 @@ require_once '../controlador/conexion.php';
  * 
  */
 class Direccion extends Conexion{
-    var $id_direccion;
-    var $calle;
-    var $numero;
-    var $ciudad;
-    var $provincia;
-    var $codigo_postal;
-    var $otros;
-    var $coordenadas;
+    private $id_direccion;
+    private $calle;
+    private $numero;
+    private $ciudad;
+    private $provincia;
+    private $codigo_postal;
+    private $otros;
+    private $coordenadas;
     
+    public function __construct() {
+        
+    }
     
+    public function getId_direccion() {
+        return $this->id_direccion;
+    }
+
+    public function getCalle() {
+        return $this->calle;
+    }
+
+    public function getNumero() {
+        return $this->numero;
+    }
+
+    public function getCiudad() {
+        return $this->ciudad;
+    }
+
+    public function getProvincia() {
+        return $this->provincia;
+    }
+
+    public function getCodigo_postal() {
+        return $this->codigo_postal;
+    }
+
+    public function getOtros() {
+        return $this->otros;
+    }
+
+    public function getCoordenadas() {
+        return $this->coordenadas;
+    }
+
+    public function setId_direccion($id_direccion): void {
+        $this->id_direccion = $id_direccion;
+    }
+
+    public function setCalle($calle): void {
+        $this->calle = $calle;
+    }
+
+    public function setNumero($numero): void {
+        $this->numero = $numero;
+    }
+
+    public function setCiudad($ciudad): void {
+        $this->ciudad = $ciudad;
+    }
+
+    public function setProvincia($provincia): void {
+        $this->provincia = $provincia;
+    }
+
+    public function setCodigo_postal($codigo_postal): void {
+        $this->codigo_postal = $codigo_postal;
+    }
+
+    public function setOtros($otros): void {
+        $this->otros = $otros;
+    }
+
+    public function setCoordenadas($coordenadas): void {
+        $this->coordenadas = $coordenadas;
+    }
+
+
 }
