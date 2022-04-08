742bd237d52c711b8447b743b6773885be425848
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-08 18:07:26 +0200

    autoCommit 20220408.180726

--- SERVIDOR/PHP/modelo/usuario.php
+++ SERVIDOR/PHP/modelo/usuario.php
@@ -1,86 +1,114 @@
 <?php
- require_once '../controlador/conexion.php';
+
+require_once '../controlador/conexion.php';
+
 /**
  * Description of usuario
  *
  * @author jmcbg
  */
 class usuario extends Conexion {
-   private $id;
-   private $nombre;
-   private $dni;
-   private $email;
-   private $tlf;
-   private $rol;
-   private $id_direccion;
-   private $password;
-   public function __construct() {
-       
-   }
-   public function getId() {
-       return $this->id;
-   }
 
-   public function getNombre() {
-       return $this->nombre;
-   }
+    private $id;
+    private $nombre;
+    private $dni;
+    private $email;
+    private $tlf;
+    private $rol;
+    private $id_direccion;
+    private $password;
 
-   public function getDni() {
-       return $this->dni;
-   }
+    public function __construct() {
+        
+    }
 
-   public function getEmail() {
-       return $this->email;
-   }
+    public function getId() {
+        return $this->id;
+    }
 
-   public function getTlf() {
-       return $this->tlf;
-   }
+    public function getNombre() {
+        return $this->nombre;
+    }
 
-   public function getRol() {
-       return $this->rol;
-   }
+    public function getDni() {
+        return $this->dni;
+    }
 
-   public function getId_direccion() {
-       return $this->id_direccion;
-   }
+    public function getEmail() {
+        return $this->email;
+    }
 
-   public function getPassword() {
-       return $this->password;
-   }
+    public function getTlf() {
+        return $this->tlf;
+    }
 
-   public function setId($id): void {
-       $this->id = $id;
-   }
+    public function getRol() {
+        return $this->rol;
+    }
 
-   public function setNombre($nombre): void {
-       $this->nombre = $nombre;
-   }
+    public function getId_direccion() {
+        return $this->id_direccion;
+    }
 
-   public function setDni($dni): void {
-       $this->dni = $dni;
-   }
+    public function getPassword() {
+        return $this->password;
+    }
 
-   public function setEmail($email): void {
-       $this->email = $email;
-   }
+    public function setId($id): void {
+        $this->id = $id;
+    }
 
-   public function setTlf($tlf): void {
-       $this->tlf = $tlf;
-   }
+    public function setNombre($nombre): void {
+        $this->nombre = $nombre;
+    }
 
-   public function setRol($rol): void {
-       $this->rol = $rol;
-   }
+    public function setDni($dni): void {
+        $this->dni = $dni;
+    }
 
-   public function setId_direccion($id_direccion): void {
-       $this->id_direccion = $id_direccion;
-   }
+    public function setEmail($email): void {
+        $this->email = $email;
+    }
 
-   public function setPassword($password): void {
-       $this->password = $password;
-   }
+    public function setTlf($tlf): void {
+        $this->tlf = $tlf;
+    }
 
+    public function setRol($rol): void {
+        $this->rol = $rol;
+    }
 
+    public function setId_direccion($id_direccion): void {
+        $this->id_direccion = $id_direccion;
+    }
+
+    public function setPassword($password): void {
+        $this->password = $password;
+    }
+
+    public function agregar() {
+        $sql = "insert into articulos(codigo, descripcion, precio) values(:cod, :des, :pre);";
+
+        $sentencia = $this->dblink->prepare($sql);
+        $codigo = $this->getCodigo();
+        $descripcion = $this->getDescripcion();
+        $precio = $this->getPrecio();
+
+        // $stmt->bindParam(':name', $userName);
+        $sentencia->bindParam(":cod", $codigo);
+        $sentencia->bindParam(":des", $descripcion);
+        $sentencia->bindParam(":pre", $precio);
+
+        // $sentencia->bindParam(":fot", $this->getFoto() );
+        $resultado = $sentencia->execute();
+
+        if ($resultado != 1) {
+            //ocurrio un error al insertar
+            return FALSE;
+        }
+
+        //Insertó correctamente
+        return TRUE;
+    }
 
 }
