94aeb4c37888ed0ea7ea6957f886bbb0c2b27728
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-04-08 18:13:20 +0200

    autoCommit 20220408.181320

--- SERVIDOR/PHP/modelo/Producto.php
+++ SERVIDOR/PHP/modelo/Producto.php
@@ -1,15 +1,70 @@
 <?php
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
- */
-
+require_once '../controlador/conexion.php';
 /**
  * Description of Producto
  *
  * @author Jesus
  */
-class Producto {
-    //put your code here
+class Producto extends Conexion{
+    private $id_producto;
+    private $id_negocio;
+    private $nombre;
+    private $precio;
+    private $descripcion;
+    private $stock;
+    
+    public function __construct() {
+        
+    }
+    
+    public function getId_producto() {
+        return $this->id_producto;
+    }
+
+    public function getId_negocio() {
+        return $this->id_negocio;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getPrecio() {
+        return $this->precio;
+    }
+
+    public function getDescripcion() {
+        return $this->descripcion;
+    }
+
+    public function getStock() {
+        return $this->stock;
+    }
+
+    public function setId_producto($id_producto): void {
+        $this->id_producto = $id_producto;
+    }
+
+    public function setId_negocio($id_negocio): void {
+        $this->id_negocio = $id_negocio;
+    }
+
+    public function setNombre($nombre): void {
+        $this->nombre = $nombre;
+    }
+
+    public function setPrecio($precio): void {
+        $this->precio = $precio;
+    }
+
+    public function setDescripcion($descripcion): void {
+        $this->descripcion = $descripcion;
+    }
+
+    public function setStock($stock): void {
+        $this->stock = $stock;
+    }
+
+
+
 }
