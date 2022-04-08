656a6862804f335d82c4958cd94ce6d07fc64216
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-04-08 18:07:12 +0200

    autoCommit 20220408.180712

--- SERVIDOR/PHP/modelo/Categoria.php
+++ SERVIDOR/PHP/modelo/Categoria.php
@@ -1,7 +1,28 @@
 <?php
+require_once '../controlador/conexion.php';
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+class Categoria extends Conexion{
+    private $id_categoria;
+    private $nombre;
+    
+    public function __construct() {
+        
+    }
+    
+    public function getId_categoria() {
+        return $this->id_categoria;
+    }
 
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function setId_categoria($id_categoria): void {
+        $this->id_categoria = $id_categoria;
+    }
+
+    public function setNombre($nombre): void {
+        $this->nombre = $nombre;
+    }
+
+}
