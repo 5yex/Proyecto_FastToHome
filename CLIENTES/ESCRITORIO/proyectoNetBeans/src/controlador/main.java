eae42d0bcb2c1e389b123a6c66c4bd72f68c8dee
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-09 13:39:18 +0200

    autoCommit 20220409.133918

--- CLIENTES/ESCRITORIO/proyectoNetBeans/src/controlador/main.java
+++ CLIENTES/ESCRITORIO/proyectoNetBeans/src/controlador/main.java
@@ -1,10 +1,28 @@
 
 package controlador;
-
 /**
  *
  * @author jmcbg
  */
 public class Main {
-    //TODO: cargar datos
+ 
+    public static void main(String[] args) throws Exception {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpUriRequest httppost = RequestBuilder.post()
+                    .setUri(new URI("https://postman-echo.com/post"))
+                    .addParameter("foo1", "bar1")
+                    .addParameter("foo2", "bar2")
+                    .build();
+ 
+            CloseableHttpResponse response = httpclient.execute(httppost);
+            try {
+                System.out.println(EntityUtils.toString(response.getEntity()));
+            } finally {
+                response.close();
+            }
+        } finally {
+            httpclient.close();
+        }
+    }
 }
