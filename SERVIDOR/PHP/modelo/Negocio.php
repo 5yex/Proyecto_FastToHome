57fb0b8a046d4dfeb81b2d83d7382ed8506c677f
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-04-11 11:41:25 +0200

    autoCommit 20220411.114125

--- SERVIDOR/PHP/modelo/Negocio.php
+++ SERVIDOR/PHP/modelo/Negocio.php
@@ -62,5 +62,7 @@ class Negocio extends Conexion{
         $this->descripcion = $descripcion;
     }
 
-
+    public function agregar() {
+        $sql = "INSERT INTO negocio (id_categoria, id_direccion, id_mercader, Nombre, Descripcion) VALUES (:cat, :dir, :mer, :nom, :des)";
+    }
 }
