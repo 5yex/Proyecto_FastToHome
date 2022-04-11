ff57d066b66591870ac2c0a0f043b005da6b2ec2
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-11 13:49:37 +0200

    autoCommit 20220411.134937

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/modelo/Producto.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/modelo/Producto.java
@@ -9,10 +9,62 @@ package modelo;
  * @author jmcbg
  */
 public class Producto {
+
     private int id_producto;
     private int id_negocio;
     private String nombre;
     private double precio;
     private String descripcion;
     private int stock;
+
+    public Producto() {
+    }
+
+    public int getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(int id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public int getId_negocio() {
+        return id_negocio;
+    }
+
+    public void setId_negocio(int id_negocio) {
+        this.id_negocio = id_negocio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
