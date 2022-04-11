c8587e79f3316f80c26b6469badb30a50459e4b4
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-11 13:49:18 +0200

    autoCommit 20220411.134918

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/modelo/Producto.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/modelo/Producto.java
@@ -9,5 +9,10 @@ package modelo;
  * @author jmcbg
  */
 public class Producto {
-    
+    private int id_producto;
+    private int id_negocio;
+    private String nombre;
+    private double precio;
+    private String descripcion;
+    private int stock;
 }
