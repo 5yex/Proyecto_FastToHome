204b52dbcb0c3852562068b4000cf131e706c3ae
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-11 13:16:19 +0200

    autoCommit 20220411.131619

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/gestion.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/gestion.java
@@ -4,10 +4,53 @@
  */
 package controlador;
 
+import com.donoso.proyectoprueba.ProyectoPrueba;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 /**
  *
  * @author jmcbg
  */
 public class gestion {
-    
+     public static String hacerConsulta(String uri, List<NameValuePair> params) {
+        String json = null;
+        try {
+            CloseableHttpClient client = HttpClients.createDefault();
+            HttpPost httpPost = new HttpPost(uri);
+
+            httpPost.setEntity(new UrlEncodedFormEntity(params));
+
+            CloseableHttpResponse response = client.execute(httpPost);
+
+            HttpEntity entity = response.getEntity();
+            Header headers = entity.getContentType();
+
+            if (entity != null) {
+                json = EntityUtils.toString(entity);
+                System.out.println(json);
+            }
+
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(ProyectoPrueba.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ProyectoPrueba.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(ProyectoPrueba.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return json;
+    }
 }
