a4a3edf43ecd56d3bfd652f95419ed94f25368bc
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-11 11:41:59 +0200

    autoCommit 20220411.114159

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/com/donoso/proyectoprueba/ProyectoPrueba.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/com/donoso/proyectoprueba/ProyectoPrueba.java
@@ -5,6 +5,9 @@
 
 package com.donoso.proyectoprueba;
 
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 /**
  *
  * @author jmcbg
@@ -12,6 +15,16 @@ package com.donoso.proyectoprueba;
 public class ProyectoPrueba {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+         CloseableHttpClient client = HttpClients.createDefault();
+    HttpPost httpPost = new HttpPost("http://www.example.com");
+
+    List<NameValuePair> params = new ArrayList<NameValuePair>();
+    params.add(new BasicNameValuePair("username", "John"));
+    params.add(new BasicNameValuePair("password", "pass"));
+    httpPost.setEntity(new UrlEncodedFormEntity(params));
+
+    CloseableHttpResponse response = client.execute(httpPost);
+    assertThat(response.getStatusLine().getStatusCode(), equalTo(200));
+    client.close();
     }
 }
