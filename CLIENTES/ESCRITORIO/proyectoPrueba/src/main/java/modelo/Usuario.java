2614eecfa67611c090c87259b01cb47ec05ed1eb
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-11 13:47:06 +0200

    autoCommit 20220411.134706

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/modelo/Usuario.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/modelo/Usuario.java
@@ -9,6 +9,7 @@ package modelo;
  * @author jmcbg
  */
 public class Usuario {
+
     private int id;
     private String nombre;
     private String dni;
@@ -17,5 +18,72 @@ public class Usuario {
     private String rol;
     private int id_direccion;
     private String password;
+
+    public Usuario() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTlf() {
+        return tlf;
+    }
+
+    public void setTlf(String tlf) {
+        this.tlf = tlf;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public int getId_direccion() {
+        return id_direccion;
+    }
+
+    public void setId_direccion(int id_direccion) {
+        this.id_direccion = id_direccion;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
