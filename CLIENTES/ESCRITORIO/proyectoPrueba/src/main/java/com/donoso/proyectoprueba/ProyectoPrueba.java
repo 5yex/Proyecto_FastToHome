258ef645c1837db35fe9ba0d27ab5c08a42b78c5
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-11 11:53:21 +0200

    autoCommit 20220411.115321

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/com/donoso/proyectoprueba/ProyectoPrueba.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/com/donoso/proyectoprueba/ProyectoPrueba.java
@@ -33,7 +33,7 @@ public class ProyectoPrueba {
 
     CloseableHttpResponse response = client.execute(httpPost);
     
-    String respuesta = response.toString();
+    String respuesta = response.getEntity().toString();
     client.close();
     }
 }
