6c1c4e81627dc6c3e3ef5ca55793d1a959e16d60
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-13 13:16:34 +0200

    autoCommit 20220413.131634

--- SERVIDOR/PHP/webService/api.php
+++ SERVIDOR/PHP/webService/api.php
@@ -9,7 +9,7 @@ if (empty($_POST["DATA"])) {
     switch ($peticion->comando) {
 
         case 'nuevo_usuario';
-            nuevoUsuario($peticion->datos);
+            nuevoUsuario(json_decode($peticion->datos));
             break;
         default;
             mandarRespuesta(true, 'comando no  reconocido');
@@ -17,10 +17,20 @@ if (empty($_POST["DATA"])) {
     }
 }
 
-
 function nuevoUsuario($datos) {
+    $datos = json_decode($datos);
     require_once '../modelo/usuario.php';
-
+//creamos usuario y los pasamos sus datos al objeto;
+    $usuario = new usuario();
+    $usuario->setNombre($nombre);
+    $usuario->setDni($dni);
+    $usuario->setEmail($email);
+    $usuario->setId_direccion($id_direccion);
+    $usuario->setTlf($tlf);
+    $usuario->setRol($rol);
+    $usuario->setPassword($password);
+//realizamos la accion
+    $resultado = $usuario->agregar();
 }
 
 function mandarRespuesta($error, $datos) {
