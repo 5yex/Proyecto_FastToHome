a2b4d06a070db76271031baf3bc2c2ca444f049e
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-13 12:28:33 +0200

    autoCommit 20220413.122833

--- SERVIDOR/PHP/webService/api.php
+++ SERVIDOR/PHP/webService/api.php
@@ -1,15 +1,23 @@
 <?php
 
-require_once '../modelo/Respuesta.php';
-
 if (empty($_POST["DATA"])) {
-    $respuesta = new Respuesta();
-    $respuesta ->setError(true);
-    $respuesta ->setDatos('llamada a api malformada');
-    
-    
-    $json = json_encode($respuesta);
+    mandarRespuesta(true, 'error en parametros');
     echo($json);
 } else {
-   
+    switch ($_POST["DATA"]) {
+        case 'tuborg';
+        case 'carlsberg';
+        case 'heineken';
+            echo 'Good choice';
+            break;
+        default;
+            echo 'Please make a new selection...';
+            break;
+    }
+}
+
+function mandarRespuesta($error, $datos) {
+    require_once '../modelo/Respuesta.php';
+    $respuesta = new Respuesta($error, $datos);
+    $json = json_encode($respuesta);
 }
