09cd1e11317a189635d05297c80e72143e60ee8b
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-13 13:35:48 +0200

    autoCommit 20220413.133548

--- SERVIDOR/PHP/webService/api.php
+++ SERVIDOR/PHP/webService/api.php
@@ -1,43 +1,49 @@
 <?php
 
-if (empty($_POST["DATA"])) {
-    mandarRespuesta(true, 'error en parametros');
-} else {
-
-    $peticion = json_decode($_POST["DATA"]);
-
-    switch ($peticion->comando) {
-
-        case 'nuevo_usuario';
-            nuevoUsuario(json_decode($peticion->datos));
-            break;
-        default;
-            mandarRespuesta(true, 'comando no  reconocido');
-            break;
-    }
-}
-
-function nuevoUsuario($datos) {
-    require_once '../modelo/usuario.php';
-
-    $usuario = new usuario();
-    $usuario->setNombre($datos->nombre);
-    $usuario->setDni($datos->dni);
-    $usuario->setEmail($datos->email);
-    $usuario->setId_direccion($datos->id_direccion);
-    $usuario->setTlf($datos->tlf);
-    $usuario->setRol($datos->rol);
-    $usuario->setPassword($datos->password);
-
-    if ($usuario->agregar()) {
-        mandarRespuesta(false, 'Se ha realizado la insercion de un usuario');
+try {
+    if (empty($_POST["DATA"])) {
+        mandarRespuesta(true, 'error en parametros');
     } else {
-        mandarRespuesta(true, 'Error en el usuario');
+
+        $peticion = json_decode($_POST["DATA"]);
+
+        switch ($peticion->comando) {
+
+            case 'nuevo_usuario';
+                nuevoUsuario(json_decode($peticion->datos));
+                break;
+            default;
+                mandarRespuesta(true, 'comando no  reconocido');
+                break;
+        }
     }
+
+    function nuevoUsuario($datos) {
+        require_once '../modelo/usuario.php';
+
+        $usuario = new usuario();
+        $usuario->setNombre($datos->nombre);
+        $usuario->setDni($datos->dni);
+        $usuario->setEmail($datos->email);
+        $usuario->setId_direccion($datos->id_direccion);
+        $usuario->setTlf($datos->tlf);
+        $usuario->setRol($datos->rol);
+        $usuario->setPassword($datos->password);
+
+        if ($usuario->agregar()) {
+            mandarRespuesta(false, 'Se ha realizado la insercion de un usuario');
+        } else {
+            mandarRespuesta(true, 'Error en el usuario');
+        }
+    }
+
+    function mandarRespuesta($error, $datos) {
+        require_once '../modelo/Respuesta.php';
+        $respuesta = new Respuesta($error, $datos);
+        echo json_encode($respuesta);
+    }
+
+} catch (PDOException $ex) {
+    mandarRespuesta(true, 'sql error');
 }
 
-function mandarRespuesta($error, $datos) {
-    require_once '../modelo/Respuesta.php';
-    $respuesta = new Respuesta($error, $datos);
-    echo json_encode($respuesta);
-}
