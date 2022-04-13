c523a581688d6f63f0e240fc89df54fdc19fcd76
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-13 12:11:21 +0200

    autoCommit 20220413.121121

--- SERVIDOR/PHP/webService/api.php
+++ SERVIDOR/PHP/webService/api.php
@@ -2,12 +2,12 @@
 
 require_once '../modelo/Respuesta.php';
 
-if (!empty($_POST["DATA"])) {
+if (empty($_POST["DATA"])) {
     $error = true;
     $datos = "llamada a api malformada";
     $respuesta = new Respuesta($error, $datos);
     echo json_encode($respuesta);
     echo 'aaa';
 } else {
-    
+   
 }
