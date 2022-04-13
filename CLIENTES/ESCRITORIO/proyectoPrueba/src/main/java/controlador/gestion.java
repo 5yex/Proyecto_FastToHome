a976fb8913f4756e66d84e001954e0f4642f7875
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-13 12:53:54 +0200

    autoCommit 20220413.125354

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/gestion.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/gestion.java
@@ -4,31 +4,26 @@
  */
 package controlador;
 
-import com.donoso.proyectoprueba.principal;
-import com.donoso.proyectoprueba.principal;
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import modelo.Peticion;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
 import org.apache.http.ParseException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Peticion;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 
 /**
  *
@@ -42,7 +37,7 @@ public class gestion {
             CloseableHttpClient client = HttpClients.createDefault();
             HttpPost httpPost = new HttpPost("http://localhost/Php/webService/api.php");
 
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            List<NameValuePair> params = new ArrayList<>();
 
             params.add(new BasicNameValuePair("DATA", new Peticion("prueba", null).getJSON()));
 
