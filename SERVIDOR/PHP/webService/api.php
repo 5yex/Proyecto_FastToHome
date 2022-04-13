483ffd0943defc075862f4d8ab3106a18a372d71
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-13 11:50:54 +0200

    autoCommit 20220413.115054

--- SERVIDOR/PHP/webService/api.php
+++ SERVIDOR/PHP/webService/api.php
@@ -1,7 +1,7 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
+if (!empty($_POST["DATA"])) {
+    echo "Yes, mail is set";
+} else {
+    echo "No, mail is not set";
+}
