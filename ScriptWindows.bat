856ac1702e39add37678ac9c0b51d217305fbb00
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-13 11:05:29 +0200

    autoCommit 20220413.110529

--- ScriptWindows.bat
+++ ScriptWindows.bat
@@ -9,5 +9,5 @@ git pull
 git add --all
 git commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
 git push
-timeout /t 1 /nobreak
+timeout /t %segundos% /nobreak
 goto :loop
