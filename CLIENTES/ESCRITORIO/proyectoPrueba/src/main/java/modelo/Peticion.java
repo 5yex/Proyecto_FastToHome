544c9c3b23aba2b78ba7cb1e9ea61c0374e19234
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-13 12:39:32 +0200

    autoCommit 20220413.123932

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/modelo/Peticion.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/modelo/Peticion.java
@@ -11,4 +11,27 @@ package modelo;
 public class Peticion {
     String comando;
     String datos;
+
+    public String getComando() {
+        return comando;
+    }
+
+    public void setComando(String comando) {
+        this.comando = comando;
+    }
+
+    public String getDatos() {
+        return datos;
+    }
+
+    public void setDatos(String datos) {
+        this.datos = datos;
+    }
+
+    public Peticion(String comando, String datos) {
+        this.comando = comando;
+        this.datos = datos;
+    }
+    
+    
 }
