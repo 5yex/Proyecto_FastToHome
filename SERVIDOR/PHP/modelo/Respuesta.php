c61535ba37be135b3e0e626f9d7c970c5f1f4e3d
José Miguel <83497667+5yex@users.noreply.github.com>
2022-04-13 11:58:47 +0200

    autoCommit 20220413.115847

--- SERVIDOR/PHP/modelo/Respuesta.php
+++ SERVIDOR/PHP/modelo/Respuesta.php
@@ -13,4 +13,25 @@
 class Respuesta {
     private $tipo; // tipos: DATOS, CONFIMRACION
     private $datos; // Si es datos, habrá un array de datos, si es Confirmacion habrá si o no.
+    public function __construct($tipo, $datos) {
+        $this->tipo = $tipo;
+        $this->datos = $datos;
+    }
+    public function getTipo() {
+        return $this->tipo;
+    }
+
+    public function getDatos() {
+        return $this->datos;
+    }
+
+    public function setTipo($tipo): void {
+        $this->tipo = $tipo;
+    }
+
+    public function setDatos($datos): void {
+        $this->datos = $datos;
+    }
+
+
 }
