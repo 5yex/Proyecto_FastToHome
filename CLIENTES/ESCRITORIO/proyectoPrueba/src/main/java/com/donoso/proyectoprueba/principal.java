6c57ad450596775b634a785b37e7fe5b7ff4ce36
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-02 13:27:26 +0200

    autoCommit 20220502.132726

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/com/donoso/proyectoprueba/principal.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/com/donoso/proyectoprueba/principal.java
@@ -4,6 +4,7 @@
  */
 package com.donoso.proyectoprueba;
 
+import controlador.gestion;
 import static controlador.gestion.hacerConsulta;
 import java.io.IOException;
 import modelo.Categoria;
@@ -19,25 +20,27 @@ public class principal {
 
     public static void main(String[] args) throws IOException {
 
-        /*Usuario user = new Usuario();
-        user.setDni("34567890a");
-        user.setEmail("fasdfa");
+        Usuario user = new Usuario();
+        user.setDni("12345678D");
+        /*user.setEmail("fasdfa");
         user.setId_direccion(0);
         user.setNombre("paco");
         user.setPassword("pruebas");
         user.setRol("clientes");
         user.setTlf("654789211");*/
         
-        Producto producto = new Producto();
+        /*Producto producto = new Producto();
         producto.setId_negocio(1);
         producto.setNombre("Manzana");
         producto.setPrecio(3.40);
         producto.setDescripcion("Manzana roja de la huerta");
-        producto.setStock(20);
+        producto.setStock(20);*/
         
         
         //System.out.println(hacerConsulta(new Peticion("nuevo_usuario", user.getJSON())));
-        System.out.println(hacerConsulta(new Peticion("nuevo_producto", producto.getJSON())));
+        //System.out.println(hacerConsulta(new Peticion("nuevo_producto", producto.getJSON())));
+        
+        System.out.println(gestion.consultaSeleccion(new Peticion("obtener_id_usuario", user.getJSON())));
     }
     
 }
