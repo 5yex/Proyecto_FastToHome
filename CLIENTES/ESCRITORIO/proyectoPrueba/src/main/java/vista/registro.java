90823e9d935a91e7d9e1f996db409198eee8f646
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-03 20:40:21 +0200

    autoCommit 20220503.204021

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/registro.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/registro.java
@@ -160,7 +160,7 @@ public class registro extends javax.swing.JFrame {
         newUsuario.setTlf(nombreField.getText());
         newUsuario.setEmail(emailField.getText());
         newUsuario.setPassword(new String(passwordField.getPassword()));
-        newUsuario.setRol("mercader");
+        newUsuario.setRol("cliente");
         System.out.println(newUsuario.getJSON());
         UsuarioDao.nuevoUsuario(newUsuario);
     }
