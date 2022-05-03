ad88b2f23c4afdff043fffe57bd3eaa4a0845d50
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-03 20:00:09 +0200

    autoCommit 20220503.200009

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/UsuarioDao.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/UsuarioDao.java
@@ -4,6 +4,7 @@
  */
 package controlador;
 
+import com.google.gson.JsonObject;
 import modelo.Peticion;
 import modelo.Usuario;
 
@@ -15,7 +16,25 @@ public class UsuarioDao {
     public static boolean nuevoUsuario(Usuario user){
          return gestion.hacerConsulta(new Peticion("nuevo_usuario", user.getJSON()));
     }
+    
+    
+    
+    
     public static String obtenerIdCliente(Usuario user){
-       return gestion.consultaSeleccion(new Peticion("obtener_id_cliente", user.getJSON())).get("id").getAsString();
+    //Lo mismo pero compacto:
+    //return gestion.consultaSeleccion(new Peticion("obtener_id_cliente", user.getJSON())).get("id").getAsString();
+
+    
+    //generamos un json con los datos que vamos a pasarle al php, en este caso solo el usuario
+    String json = user.getJSON();
+    //hacemos una petición con el comando que deberá realizar el php, y los datos en json    
+    Peticion peticion = new Peticion("obtener_id_cliente",json);
+    //mandamos la peticion como consulta selección para obtener valores
+    JsonObject respuesta = gestion.consultaSeleccion(peticion);
+    //de la respuesta, obtenemos el id
+    String idCliente =  respuesta.get("id").getAsString();
+    
+    return idCliente;
+    
     }
 }
