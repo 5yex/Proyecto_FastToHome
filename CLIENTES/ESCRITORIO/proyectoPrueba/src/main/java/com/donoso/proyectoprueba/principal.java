73cebc5901672f09fbf926c6b83b291dca218dcd
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-04 19:46:14 +0200

    autoCommit 20220504.194614

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/com/donoso/proyectoprueba/principal.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/com/donoso/proyectoprueba/principal.java
@@ -12,6 +12,7 @@ import modelo.Categoria;
 import modelo.Peticion;
 import modelo.Producto;
 import modelo.Usuario;
+import org.mindrot.jbcrypt.BCrypt;
 
 /**
  *
@@ -29,19 +30,28 @@ public class principal {
         user.setPassword("pruebas");
         user.setRol("clientes");
         user.setTlf("654789211");*/
-        
-        /*Producto producto = new Producto();
+
+ /*Producto producto = new Producto();
         producto.setId_negocio(1);
         producto.setNombre("Manzana");
         producto.setPrecio(3.40);
         producto.setDescripcion("Manzana roja de la huerta");
         producto.setStock(20);*/
-        
-        
         //System.out.println(hacerConsulta(new Peticion("nuevo_usuario", user.getJSON())));
         //System.out.println(hacerConsulta(new Peticion("nuevo_producto", producto.getJSON())));
-        
-        System.out.println(UsuarioDao.obtenerIdCliente(user));
+        //System.out.println(UsuarioDao.obtenerIdCliente(user));
+        if (BCrypt.checkpw("PACO", "$2a$10$jIukrdC3Om1/OXSCfVmUl.9SToQjOzL48AgdPYcfTbqmFRHNrfHTS")) {
+            System.out.println("LA CONTRASEÑA SE HA GENERADO CORRECTAMENTE Wey");
+        }
+        if (BCrypt.checkpw("PACO", "$2a$10$7pPIorywzYnthCFpvoPuHuNtN997fkmbJTlS/LfEnCIv48BtmJ7Hm")) {
+            System.out.println("LA CONTRASEÑA SE HA GENERADO CORRECTAMENTE Wey");
+        }
+        if (BCrypt.checkpw("PACO", "$2a$10$36VUzMqDJ9F/8j7n32pcPOoCW2UAh.23uESbHDYowyv7kClLa25/C")) {
+            System.out.println("LA CONTRASEÑA SE HA GENERADO CORRECTAMENTE Wey");
+        }
+        if (BCrypt.checkpw("PACO", "$2a$10$uv1n0lPuPv1V7/C/6wdn..y7v2SPiKMEN15iIbHS/WYnAlTWa6rIG")) {
+            System.out.println("LA CONTRASEÑA SE HA GENERADO CORRECTAMENTE Wey");
+        }
     }
-    
+
 }
