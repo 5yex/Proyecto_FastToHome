7cf4db4b9776f1e68355ab46ac6763023b9841dc
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-06 19:18:09 +0200

    autoCommit 20220506.191809

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/login.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/login.java
@@ -123,7 +123,6 @@ public class login extends javax.swing.JFrame implements util.Constantes {
     }// </editor-fold>//GEN-END:initComponents
 
     private void buttonLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonLoginActionPerformed
-        this.setVisible(false);
         new registro().setVisible(true);
     }//GEN-LAST:event_buttonLoginActionPerformed
 
