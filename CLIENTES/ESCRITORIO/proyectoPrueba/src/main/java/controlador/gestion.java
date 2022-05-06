9f5d24ae155b2fb51f6578c92ad846677c3f1dea
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-06 16:47:31 +0200

    autoCommit 20220506.164731

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/gestion.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/gestion.java
@@ -22,8 +22,6 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import modelo.Peticion;
 import org.apache.http.NameValuePair;
@@ -79,9 +77,8 @@ public class gestion {
      * atributos
      */
     public static JsonObject consultaSeleccion(Peticion peticion) {
+        String json = null;
         try {
-            String json = null;
-            
             CloseableHttpClient client = HttpClients.createDefault();
             HttpPost httpPost = new HttpPost("http://localhost/Php/webService/api.php");
 
@@ -117,15 +114,10 @@ public class gestion {
                 }
             }
             return null;
-        } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(gestion.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(gestion.class.getName()).log(Level.SEVERE, null, ex);
+
         } catch (Exception ex) {
-            Logger.getLogger(gestion.class.getName()).log(Level.SEVERE, null, ex);
-        } finally{
+             JOptionPane.showMessageDialog(null,ex.getMessage());
             return null;
         }
-            
     }
 }
