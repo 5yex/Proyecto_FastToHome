189b112e3898bf983c12871deed2fe78b376484b
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-06 17:07:51 +0200

    autoCommit 20220506.170751

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/UsuarioDao.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/UsuarioDao.java
@@ -13,62 +13,51 @@ import modelo.Usuario;
  * @author jmcbg
  */
 public class UsuarioDao {
-    public static boolean nuevoUsuario(Usuario user){
-         return gestion.hacerConsulta(new Peticion("nuevo_usuario", user.getJSON()));
-    }
-    
-    
-    
-    
-    public static String obtenerIdCliente(Usuario user){
-    //Lo mismo pero compacto:
-    //return gestion.consultaSeleccion(new Peticion("obtener_id_cliente", user.getJSON())).get("id").getAsString();
 
-    
-    //generamos un json con los datos que vamos a pasarle al php, en este caso solo el usuario
-    String json = user.getJSON();
-    //hacemos una petición con el comando que deberá realizar el php, y los datos en json    
-    Peticion peticion = new Peticion("obtener_id_cliente",json);
-    //mandamos la peticion como consulta selección para obtener valores
-    JsonObject respuesta = gestion.consultaSeleccion(peticion);
-    //de la respuesta, obtenemos el id
-    String idCliente =  respuesta.get("id").getAsString();
-    
-    return idCliente;
-    
+    public static boolean nuevoUsuario(Usuario user) {
+        return gestion.hacerConsulta(new Peticion("nuevo_usuario", user.getJSON()));
     }
-    
-    
-    public static String login(Usuario user){
-   
-    return gestion.consultaSeleccion(new Peticion("user_login", user.getJSON())).get("password").getAsString();
 
-    
+    public static String obtenerIdCliente(Usuario user) {
+        //Lo mismo pero compacto:
+        //return gestion.consultaSeleccion(new Peticion("obtener_id_cliente", user.getJSON())).get("id").getAsString();
+
+        //generamos un json con los datos que vamos a pasarle al php, en este caso solo el usuario
+        String json = user.getJSON();
+        //hacemos una petición con el comando que deberá realizar el php, y los datos en json    
+        Peticion peticion = new Peticion("obtener_id_cliente", json);
+        //mandamos la peticion como consulta selección para obtener valores
+        JsonObject respuesta = gestion.consultaSeleccion(peticion);
+        //de la respuesta, obtenemos el id
+        String idCliente = respuesta.get("id").getAsString();
+
+        return idCliente;
+
     }
-    
-     
-    public static Usuario obtenerDatosUsuario(Usuario user){
-    //generamos un json con los datos que vamos a pasarle al php, en este caso solo el usuario
-    String json = user.getJSON();
-    //hacemos una petición con el comando que deberá realizar el php, y los datos en json    
-    Peticion peticion = new Peticion("obtener_cliente",json);
-    //mandamos la peticion como consulta selección para obtener valores
-    JsonObject respuesta = gestion.consultaSeleccion(peticion);
-    //de la respuesta, obtenemos el id
-   
-    
-    user.setDni(respuesta.get("Dni").getAsString());
-    user.setEmail(respuesta.get("Email").getAsString());
-    user.setId(respuesta.get("id").getAsInt());
-    user.setId_direccion(respuesta.get("direccion_id").getAsInt());
-    user.setNombre(respuesta.get("Nombre").getAsString());
-    user.setPassword(respuesta.get("password").getAsString());
-    user.setRol(respuesta.get("Rol").getAsString());
-    user.setTlf(respuesta.get("tlf").getAsString());
-  
-    
-    return user;
-    
+
+    public static String login(Usuario user) {
+
+        return gestion.consultaSeleccion(new Peticion("user_login", user.getJSON())).get("password").getAsString();
+
     }
-    
+
+    public static Usuario obtenerDatosUsuario(Usuario user) {
+        //generamos un json con los datos que vamos a pasarle al php, en este caso solo el usuario
+        String json = user.getJSON();
+        //hacemos una petición con el comando que deberá realizar el php, y los datos en json    
+        Peticion peticion = new Peticion("obtener_cliente", json);
+        //mandamos la peticion como consulta selección para obtener valores
+        JsonObject respuesta = gestion.consultaSeleccion(peticion);
+        //de la respuesta, obtenemos el id    
+        user.setDni(respuesta.get("Dni").getAsString());
+        user.setEmail(respuesta.get("Email").getAsString());
+        user.setId(respuesta.get("id").getAsInt());
+        user.setId_direccion(respuesta.get("direccion_id").getAsInt());
+        user.setNombre(respuesta.get("Nombre").getAsString());
+        user.setPassword(respuesta.get("password").getAsString());
+        user.setRol(respuesta.get("Rol").getAsString());
+        user.setTlf(respuesta.get("tlf").getAsString());
+        return user;
+    }
+
 }
