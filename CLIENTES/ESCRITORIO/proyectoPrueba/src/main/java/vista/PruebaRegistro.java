00e877f3a01404b33d5799d86a71ebfddb5ec2a1
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-07 21:01:27 +0200

    autoCommit 20220507.210127

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/PruebaRegistro.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/PruebaRegistro.java
@@ -202,7 +202,9 @@ public class PruebaRegistro extends javax.swing.JFrame implements Constantes{
     }//GEN-LAST:event_dniFieldFocusLost
 
     private void tlfFieldFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_tlfFieldFocusLost
-        if(!validaciones.validar(tlfField.getText(), PATRON_TELEFONO)
+        if(!validaciones.validar(tlfField.getText(), PATRON_TELEFONO)){
+            
+        }
     }//GEN-LAST:event_tlfFieldFocusLost
 
     private void registrarUsuario() {
