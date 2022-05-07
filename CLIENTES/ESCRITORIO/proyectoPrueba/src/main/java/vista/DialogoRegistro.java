47adb518e8a1a3ba7449cd6252359eafd107bec2
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-08 01:36:49 +0200

    autoCommit 20220508. 13649

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/DialogoRegistro.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/DialogoRegistro.java
@@ -224,7 +224,7 @@ public class DialogoRegistro extends javax.swing.JDialog implements Constantes{
     private void nombreFieldFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_nombreFieldFocusLost
         if(!validaciones.validar(nombreField.getText(),PATRON_NOMBRES)){
             //JOptionPane.showMessageDialog(this, MENSAJE_NOMBRE_INFO, "Error", JOptionPane.ERROR_MESSAGE);
-            nombreField.setFont(Color.RED)
+            nombreField.setFont(Color.RED);
             nombreField.setText(MENSAJE_NOMBRE_INFO);
         }
     }//GEN-LAST:event_nombreFieldFocusLost
