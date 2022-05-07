d2451ca81d25d947ef44d617808e9efab038c2be
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-07 11:01:03 +0200

    autoCommit 20220507.110103

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/util/Constantes.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/util/Constantes.java
@@ -51,7 +51,7 @@ public interface Constantes {
     final public String PATRON_ANIO = "^(2020|" + ANIO_ACTUAL + ")$";
     final public String PATRON_PRECIO_KILO = "^0\\.(?!0+$)\\d{1,2}$";
     final public String PATRON_DNI = "(([x-z]|[X-Z]{1})([-]?)(\\d{7})([-]?)([a-z]|[A-Z]{1}))|((\\d{8})([-]?)([a-z]|[A-Z]{1}))";
-    final public String PATRON_NOMBRES = "[A-Za-z ]{2,15}";
+    final public String PATRON_NOMBRES = "[A-Za-z ]{2,30}";
     final public String PATRON_EDAD = "^(1[8-9]|[2-9][0-9])$";
     final public String PATRON_KILOS = "^\\d*(\\.\\d{1})?\\d{0,1}$";
     final public String PATRON_FINCA = "^([a-zA-Z ]{3,30})$";
