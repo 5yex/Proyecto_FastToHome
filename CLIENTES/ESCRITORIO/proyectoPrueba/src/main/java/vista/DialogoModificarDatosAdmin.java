14a0040b8858f7e28f101ccab29b9cc5f3e85c75
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-08 17:00:17 +0200

    autoCommit 20220508.170017

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/DialogoModificarDatosAdmin.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/DialogoModificarDatosAdmin.java
@@ -25,7 +25,7 @@ public class DialogoModificarDatosAdmin extends javax.swing.JDialog implements C
     /**
      * Creates new form DialogoRegistro
      */
-    public DialogoModificarDatosAdmin(java.awt.Frame parent, boolean modal, Usuario administrador) {
+    public DialogoModificarDatosAdmin(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
     }
@@ -386,7 +386,7 @@ public class DialogoModificarDatosAdmin extends javax.swing.JDialog implements C
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                DialogoModificarDatosAdmin dialog = new DialogoModificarDatosAdmin(new javax.swing.JFrame(), true, admin);
+                DialogoModificarDatosAdmin dialog = new DialogoModificarDatosAdmin(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
