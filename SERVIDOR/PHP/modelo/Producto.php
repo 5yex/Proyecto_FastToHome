8e3633c28d5a4d38287a092c731648660da999c8
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-10 20:36:15 +0200

    autoCommit 20220510.203615

--- SERVIDOR/PHP/modelo/Producto.php
+++ SERVIDOR/PHP/modelo/Producto.php
@@ -100,7 +100,10 @@ class Producto extends Conexion{
         $sentencia = $this->dblink->prepare($sql);
 
         $id_negocio = $this->getId_negocio();
-        $sentencia->bindParam(":id_neg, $id_negocio);
+        $sentencia->bindParam(":id", $id);
+
+        $pass = $this->getPassword();
+        $sentencia->bindParam(":pass", $pass);
 
         $sentencia->execute();
         return $sentencia->fetchAll(PDO::FETCH_OBJ);
