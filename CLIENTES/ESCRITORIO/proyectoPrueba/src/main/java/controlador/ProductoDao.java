c489b989ae6dff14285fa5810d10f37b2b1f1723
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-11 09:23:02 +0200

    autoCommit 20220511. 92301

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/ProductoDao.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/ProductoDao.java
@@ -6,6 +6,8 @@
  */
 package controlador;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import java.util.ArrayList;
 import modelo.Negocio;
 import modelo.Peticion;
@@ -20,7 +22,28 @@ public class ProductoDao {
         return gestion.hacerConsulta(new Peticion("nuevo_producto", product.getJSON()));
     }
     
-    public ArrayList <Producto> selecciónProductosNegocio(Negocio negocio){
-        
+    public ArrayList <Producto> selecciónProductosNegocio(Producto product){
+        JsonArray jsonArray = gestion.consultaSeleccion(new Peticion("obtener_usuarios", product.getJSON()));
+
+        ArrayList<Producto> listaProducto = new ArrayList<Producto>();
+
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JsonObject usuarioJson = jsonArray.get(i).getAsJsonObject();
+
+            /*user.setDni(usuarioJson.get("Dni").getAsString());
+            user.setEmail(usuarioJson.get("Email").getAsString());
+            user.setId(usuarioJson.get("id").getAsInt());
+            if (!usuarioJson.get("direccion_id").isJsonNull()) {
+                user.setId_direccion(usuarioJson.get("direccion_id").getAsInt());
+            }
+            user.setNombre(usuarioJson.get("Nombre").getAsString());
+            user.setPassword(usuarioJson.get("password").getAsString());
+            user.setRol(usuarioJson.get("Rol").getAsString());
+            user.setTlf(usuarioJson.get("tlf").getAsString());
+            
+            listaUsuarios.add(user);*/
+
+        }
+        return listaUsuarios;
     }
 }
