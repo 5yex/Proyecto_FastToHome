3fb5eb2cd583dea3ece97b92c8a7ff693e4d6bd5
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-13 19:05:24 +0200

    autoCommit 20220513.190524

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/VentanaMercader.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/VentanaMercader.java
@@ -45,7 +45,9 @@ public class VentanaMercader extends javax.swing.JFrame {
         setTitle("Ventana Mercader");
         setMinimumSize(new java.awt.Dimension(600, 400));
 
+        jTabbedPane1.setBackground(new java.awt.Color(255, 204, 204));
         jTabbedPane1.setTabPlacement(javax.swing.JTabbedPane.LEFT);
+        jTabbedPane1.setOpaque(true);
         jTabbedPane1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jTabbedPane1MouseClicked(evt);
