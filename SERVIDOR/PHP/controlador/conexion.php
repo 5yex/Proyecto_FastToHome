b5702e00f36ecce10a52f047c622cff5fac5392a
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-13 14:02:11 +0200

    autoCommit 20220513.140211

--- SERVIDOR/PHP/controlador/conexion.php
+++ SERVIDOR/PHP/controlador/conexion.php
@@ -3,9 +3,14 @@
         var $dblink;
                 
         function __construct() {
-            $servidor = "mysql:host=localhost;dbname=pf";
-            $usuario = "root";
-            $clave = "";
+            //$servidor = "mysql:host=localhost;dbname=pf";
+            //$usuario = "root";
+           // $clave = "";
+            
+            $servidor = "mysql:host=serviex.duckdns.org;dbname=pf";
+            $usuario = "proyecto";
+            $clave = "123456";
+            
             
             $this->dblink = new PDO($servidor, $usuario, $clave);
             $this->dblink->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
