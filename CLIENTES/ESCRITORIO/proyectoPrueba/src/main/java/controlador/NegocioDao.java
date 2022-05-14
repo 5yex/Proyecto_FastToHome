642c260c6b56253136e8b8ac9a0382555e9a6208
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-14 19:41:58 +0200

    autoCommit 20220514.194158

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/NegocioDao.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/NegocioDao.java
@@ -22,11 +22,11 @@ public class NegocioDao {
         if(respuesta == null){
             return null;
         }
-        negocio.setId_direccion(respuesta.get("id_direccion").getAsInt());
+        negocio.setId_direccion(respuesta.get("direccion_id").getAsInt());
         negocio.setId_categoria(respuesta.get("id_categoria").getAsInt());    
         negocio.setId_mercader(respuesta.get("id_mercader").getAsInt());
-        negocio.setNombre(respuesta.get("nombre").getAsString());
-        negocio.setDescripcion(respuesta.get("descripcion").getAsString());
+        negocio.setNombre(respuesta.get("Nombre").getAsString());
+        negocio.setDescripcion(respuesta.get("Descripcion").getAsString());
 
         return negocio;
     }
