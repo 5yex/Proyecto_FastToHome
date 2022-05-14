e1537367396206ad6c8af2db6db33c1adca5065e
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-14 17:34:21 +0200

    autoCommit 20220514.173421

--- CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaInicio.java
+++ CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaInicio.java
@@ -2,7 +2,9 @@ package com.donosocortes.fast2home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class PantallaInicio extends AppCompatActivity {
@@ -12,4 +14,10 @@ public class PantallaInicio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void patallaLogin(View view) {
+        Intent i = new Intent(this, PantallaLogin.class );
+        startActivity(i);
+    }
+
 }
