abb16dd58f4dc1ea3ed85a2c80c40968fc19a23b
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-14 19:10:41 +0200

    autoCommit 20220514.191041

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/NegocioDao.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/NegocioDao.java
@@ -9,17 +9,17 @@ import modelo.Usuario;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author jmcbg
  */
 public class NegocioDao {
-    
-     public static negocio negocioDeMercader(Usuario user){
-        return gestion.consultaSeleccionUnico(new Peticion("mercader_de_usuario", user.getJSON()));
+
+    public static Negocio negocioDeMercader(Usuario user) {
+        Negocio negocio;
+        JsonObject respuesta = gestion.consultaSeleccionUnico(new Peticion("mercader_de_usuario", user.getJSON()));
+
+        return negocio;
     }
-    
-    
-    
+
 }
