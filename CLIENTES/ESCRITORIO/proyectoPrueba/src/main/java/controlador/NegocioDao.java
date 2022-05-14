14dd4c623fc1cbdfb377ba709f458a204a634e56
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-14 19:06:09 +0200

    autoCommit 20220514.190609

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/NegocioDao.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/NegocioDao.java
@@ -3,6 +3,7 @@ package controlador;
 import com.google.gson.JsonObject;
 import modelo.Negocio;
 import modelo.Peticion;
+import modelo.Usuario;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
