985329c95c31cb15d27c1c10126f1949e2fe475f
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-14 17:30:49 +0200

    autoCommit 20220514.173049

---
+++ CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaLogin.java
@@ -0,0 +1,14 @@
+package com.donosocortes.fast2home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_login);
+    }
+}
