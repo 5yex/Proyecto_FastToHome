acd206920b31d448d9dbe82843227942ab564534
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-14 19:16:29 +0200

    autoCommit 20220514.191629

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/NegocioDao.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/NegocioDao.java
@@ -19,6 +19,10 @@ public class NegocioDao {
         Negocio negocio = new Negocio();
         JsonObject respuesta = gestion.consultaSeleccionUnico(new Peticion("mercader_de_usuario", user.getJSON()));
 
+        if(respuesta == null){
+            return null;
+        }
+        
         negocio.setId_direccion(respuesta.get("id_direccion").getAsInt());
         negocio.setId_categoria(respuesta.get("$id_categoria").getAsInt());    
         negocio.setId_mercader(respuesta.get("$id_mercader").getAsInt());
