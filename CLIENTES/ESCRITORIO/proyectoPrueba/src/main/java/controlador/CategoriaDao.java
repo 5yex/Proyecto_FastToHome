bf5552b7ebc306d0cad11c294c6c81e4f03601bc
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-15 14:12:40 +0200

    autoCommit 20220515.141240

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/CategoriaDao.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/CategoriaDao.java
@@ -4,6 +4,8 @@
  */
 package controlador;
 
+import com.google.gson.JsonArray;
+import java.util.ArrayList;
 import modelo.Categoria;
 import modelo.Peticion;
 
@@ -17,5 +19,29 @@ public class CategoriaDao {
         return gestion.hacerConsulta(new Peticion("nueva_categoria", categoria.getJSON()));
     }
     
-    
+    public static ArrayList<Categoria> obtenerCategorias(Categoria categoria){
+        
+        JsonArray jsonArray = gestion.consultaSeleccion(new Peticion("obtener_categorias", categoria.getJSON()));
+
+        ArrayList<Categoria> listaCategorias = new ArrayList<Categoria>();
+
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JsonObject usuarioJson = jsonArray.get(i).getAsJsonObject();
+
+            user.setDni(usuarioJson.get("Dni").getAsString());
+            user.setEmail(usuarioJson.get("Email").getAsString());
+            user.setId(usuarioJson.get("id").getAsInt());
+            if (!usuarioJson.get("direccion_id").isJsonNull()) {
+                user.setId_direccion(usuarioJson.get("direccion_id").getAsInt());
+            }
+            user.setNombre(usuarioJson.get("Nombre").getAsString());
+            user.setPassword(usuarioJson.get("password").getAsString());
+            //user.setRol(usuarioJson.get("Rol").getAsString());
+            user.setTlf(usuarioJson.get("tlf").getAsString());
+            
+            listaCategorias.add(user);
+
+        }
+        return listaCategorias;
+    }
 }
