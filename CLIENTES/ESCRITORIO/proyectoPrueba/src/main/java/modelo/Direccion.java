c8716ef79a4b1ae70d53bf492246502be6281e90
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-15 13:50:57 +0200

    autoCommit 20220515.135057

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/modelo/Direccion.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/modelo/Direccion.java
@@ -20,4 +20,80 @@ public class Direccion {
    private int codigo_postal;
    private int coordenads;
 
+    public Direccion(int id_direccion, String calle, int numero, String ciudad, String otros, int codigo_postal, int coordenads) {
+        this.id_direccion = id_direccion;
+        this.calle = calle;
+        this.numero = numero;
+        this.ciudad = ciudad;
+        this.otros = otros;
+        this.codigo_postal = codigo_postal;
+        this.coordenads = coordenads;
+    }
+
+    public Direccion(String calle, int numero, String ciudad, String otros, int codigo_postal, int coordenads) {
+        this.calle = calle;
+        this.numero = numero;
+        this.ciudad = ciudad;
+        this.otros = otros;
+        this.codigo_postal = codigo_postal;
+        this.coordenads = coordenads;
+    }
+
+    public int getId_direccion() {
+        return id_direccion;
+    }
+
+    public void setId_direccion(int id_direccion) {
+        this.id_direccion = id_direccion;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getOtros() {
+        return otros;
+    }
+
+    public void setOtros(String otros) {
+        this.otros = otros;
+    }
+
+    public int getCodigo_postal() {
+        return codigo_postal;
+    }
+
+    public void setCodigo_postal(int codigo_postal) {
+        this.codigo_postal = codigo_postal;
+    }
+
+    public int getCoordenads() {
+        return coordenads;
+    }
+
+    public void setCoordenads(int coordenads) {
+        this.coordenads = coordenads;
+    }
+   
+   
 }
