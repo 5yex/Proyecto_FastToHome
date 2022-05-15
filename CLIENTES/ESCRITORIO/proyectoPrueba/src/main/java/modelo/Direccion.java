75168107e455dfaefb79805944adda8b1cd61943
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-15 13:50:11 +0200

    autoCommit 20220515.135011

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/modelo/Direccion.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/modelo/Direccion.java
@@ -11,51 +11,13 @@ import com.google.gson.Gson;
  * @author jmcbg
  */
 public class Direccion {
+    
    private int id_direccion;
-   private String Direccion;
+   private String calle;
+   private int numero;
+   private String ciudad;
    private String otros;
-   private String coordenads;
-
-    public Direccion() {
-    }
-
-    public int getId_direccion() {
-        return id_direccion;
-    }
-
-    public void setId_direccion(int id_direccion) {
-        this.id_direccion = id_direccion;
-    }
-
-    public String getDireccion() {
-        return Direccion;
-    }
-
-    public void setDireccion(String Direccion) {
-        this.Direccion = Direccion;
-    }
-
-    public String getOtros() {
-        return otros;
-    }
-
-    public void setOtros(String otros) {
-        this.otros = otros;
-    }
-
-    public String getCoordenads() {
-        return coordenads;
-    }
-
-    public void setCoordenads(String coordenads) {
-        this.coordenads = coordenads;
-    }
-
-    public Direccion(int id_direccion, String Direccion, String otros, String coordenads) {
-        this.id_direccion = id_direccion;
-        this.Direccion = Direccion;
-        this.otros = otros;
-        this.coordenads = coordenads;
-    }
+   private int codigo_postal;
+   private int coordenads;
 
 }
