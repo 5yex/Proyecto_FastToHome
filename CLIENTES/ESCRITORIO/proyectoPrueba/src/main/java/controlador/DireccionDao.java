07e0dafbdb40c299de167c0432a4e2108d26241c
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-15 18:20:43 +0200

    autoCommit 20220515.182043

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/DireccionDao.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/DireccionDao.java
@@ -4,6 +4,7 @@
  */
 package controlador;
 
+import com.google.gson.JsonObject;
 import modelo.Direccion;
 import modelo.Negocio;
 import modelo.Peticion;
@@ -13,15 +14,19 @@ import modelo.Peticion;
  * @author Jesus
  */
 public class DireccionDao {
-    
+
     public static boolean nuevaDireccion(Direccion direccion) {
         return gestion.hacerConsulta(new Peticion("nueva_direccion", direccion.getJSON()));
     }
-    
+
     public static Direccion obtenerDatosDireccionNegocio(Negocio negocio) {
-       Direccion direccion = new Direccion();
-       
-       
+        Direccion direccion = new Direccion();
+
+        String json = negocio.getJSON();
+        //hacemos una petición con el comando que deberá realizar el php, y los datos en json    
+        Peticion peticion = new Peticion("getUsuario", json);
+        //mandamos la peticion como consulta selección para obtener valores
+        JsonObject respuesta = gestion.consultaSeleccionUnico(peticion);
     }
-    
+
 }
