2f13f9d07c122a08630ecee12fb1f03cb42abccc
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-15 16:50:37 +0200

    autoCommit 20220515.165037

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/registroNegocio.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/registroNegocio.java
@@ -138,6 +138,7 @@ public class registroNegocio extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void botonPedirDireccionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonPedirDireccionActionPerformed
