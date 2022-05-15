26dd902865cdbb03b8190e8a5a1cea8e51dae401
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-15 17:38:34 +0200

    autoCommit 20220515.173834

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/CategoriaDao.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/CategoriaDao.java
@@ -15,24 +15,23 @@ import modelo.Peticion;
  * @author Jesus
  */
 public class CategoriaDao {
-    
+
     public static boolean nuevoUsuario(Categoria categoria) {
         return gestion.hacerConsulta(new Peticion("nueva_categoria", categoria.getJSON()));
     }
-    
-    public static ArrayList<Categoria> obtenerCategorias(){
-        JsonArray jsonArray = gestion.consultaSeleccion(new Peticion("obtener_categorias", categoria.getJSON()));
+
+    public static ArrayList<Categoria> obtenerCategorias() {
+        JsonArray jsonArray = gestion.consultaSeleccion(new Peticion("obtener_categorias", null));
 
         ArrayList<Categoria> listaCategorias = new ArrayList<Categoria>();
 
         for (int i = 0; i < jsonArray.size(); i++) {
-                    Categoria categoria = new Categoria();
-
+            Categoria categoria = new Categoria();
             JsonObject categoriaJson = jsonArray.get(i).getAsJsonObject();
 
             categoria.setId(categoriaJson.get("id").getAsInt());
             categoria.setNombre(categoriaJson.get("Nombre").getAsString());
-            
+
             listaCategorias.add(categoria);
 
         }
