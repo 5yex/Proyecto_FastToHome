fdf8004f7780800513606ae9f7de195913ffc677
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-18 22:22:47 +0200

    autoCommit 20220518.222247

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/registrarUsuario.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/registrarUsuario.java
@@ -368,36 +368,7 @@ public class registrarUsuario extends javax.swing.JDialog implements Constantes{
     }//GEN-LAST:event_nombreFieldActionPerformed
     
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            javax.swing.UIManager.setLookAndFeel(new FlatDarkLaf());
-        } catch (UnsupportedLookAndFeelException ex) {
-
-        }
-        //</editor-fold>
-
-        /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                registrarUsuario dialog = new registrarUsuario(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
-    }
+ 
     
     private void registrarUsuario() {
         newUsuario.setNombre(nombreField.getText());
