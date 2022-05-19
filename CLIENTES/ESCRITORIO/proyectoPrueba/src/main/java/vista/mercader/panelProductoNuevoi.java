b83f591c217aacea3d6ea544f7e761b63172ab1b
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-19 21:04:01 +0200

    autoCommit 20220519.210401

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/panelProductoNuevoi.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/panelProductoNuevoi.java
@@ -17,7 +17,6 @@ public class panelProductoNuevoi extends javax.swing.JPanel {
      */
     public panelProductoNuevoi() {
         initComponents();
-        producto = prod;
 
     }
 
@@ -31,74 +30,30 @@ public class panelProductoNuevoi extends javax.swing.JPanel {
     private void initComponents() {
 
         nombreProducto = new javax.swing.JLabel();
-        imgProducto = new javax.swing.JLabel();
-        botonEliminarProducto = new javax.swing.JButton();
-        botonEditarProducto = new javax.swing.JButton();
 
         nombreProducto.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        nombreProducto.setText("TITULO PRODUCTO");
-
-        imgProducto.setBackground(new java.awt.Color(204, 204, 255));
-        imgProducto.setText("IMAGEN PRODUCTO");
-        imgProducto.setOpaque(true);
-
-        botonEliminarProducto.setText("DEL");
-        botonEliminarProducto.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                botonEliminarProductoActionPerformed(evt);
-            }
-        });
-
-        botonEditarProducto.setText("EDIT");
-        botonEditarProducto.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                botonEditarProductoActionPerformed(evt);
-            }
-        });
+        nombreProducto.setText("NUEVO");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(nombreProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                    .addComponent(imgProducto, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(botonEliminarProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(botonEditarProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(38, Short.MAX_VALUE)
+                .addComponent(nombreProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
+                .addContainerGap(104, Short.MAX_VALUE)
                 .addComponent(nombreProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(imgProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(botonEliminarProducto, javax.swing.GroupLayout.DEFAULT_SIZE, 34, Short.MAX_VALUE)
-                    .addComponent(botonEditarProducto, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap())
+                .addGap(90, 90, 90))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void botonEliminarProductoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonEliminarProductoActionPerformed
-       eliminarProducto();
-    }//GEN-LAST:event_botonEliminarProductoActionPerformed
-
-    private void botonEditarProductoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonEditarProductoActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_botonEditarProductoActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton botonEditarProducto;
-    private javax.swing.JButton botonEliminarProducto;
-    private javax.swing.JLabel imgProducto;
     private javax.swing.JLabel nombreProducto;
     // End of variables declaration//GEN-END:variables
 
