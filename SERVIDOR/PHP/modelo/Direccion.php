31a3d945c468b5c9a64cc17c650ce32cccc81272
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-19 21:13:55 +0200

    autoCommit 20220519.211355

--- SERVIDOR/PHP/modelo/Direccion.php
+++ SERVIDOR/PHP/modelo/Direccion.php
@@ -131,7 +131,33 @@ class Direccion extends Conexion{
     }
     
     public function actualizarDireccion(){
-        $sql = "UPDATE direccion SET Calle = :calle, Numero = :num, Ciudad = :ciu, CP = :cp, Otros = :otros, Coordenadas = :coor WHERE id = :id_dir"
+        $sql = "UPDATE direccion SET Calle = :calle, Numero = :num, Ciudad = :ciu, CP = :cp, Otros = :otros, Coordenadas = :coor WHERE id = :id_dir";
+        
+        $sentencia = $this->dblink->prepare($sql);
+        
+        $id_direccion = $this->getId_direccion();
+        $calle = $this->getCalle();
+        $numero = $this->getNumero();
+        $ciudad = $this->getCiudad();
+        $codigo_postal = $this->getCodigo_postal();
+        $otros = $this->getOtros();
+        $coordenadas = $this->getCoordenadas();
+        
+        $sentencia->bindParam(":id_dir", $id_direccion);
+        $sentencia->bindParam(":calle", $calle);
+        $sentencia->bindParam(":num", $numero);
+        $sentencia->bindParam(":ciu", $ciudad);
+        $sentencia->bindParam(":cp", $codigo_postal);
+        $sentencia->bindParam(":otros", $otros);
+        $sentencia->bindParam(":coor", $coordenadas);
+        
+        $resultado = $sentencia->execute();
+ 
+        if ($resultado != 1) {
+            return FALSE;
+        }
+        //Insertó correctamente
+        return TRUE;
     }
     
 }
