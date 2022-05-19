7a7e9e11c53e73fb78f0cfb2f3c64a48d6bb8fca
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-19 20:52:57 +0200

    autoCommit 20220519.205257

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/administrador/DialogoModificarDatosAdmin.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/administrador/DialogoModificarDatosAdmin.java
@@ -397,7 +397,7 @@ public class DialogoModificarDatosAdmin extends javax.swing.JDialog implements C
     }//GEN-LAST:event_passwordConfirmFieldActionPerformed
 
     private void botonModificarDireccionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonModificarDireccionActionPerformed
-        Direccion direccionAdmin = DireccionDao.obtenerDireccionUsuario(admin);
+        direccionAdmin = DireccionDao.obtenerDireccionUsuario(admin);
         DialogoNuevaDireccion dialogoDireccion  = new DialogoNuevaDireccion(this, true, direccionAdmin);
         dialogoDireccion.setVisible(true);
     }//GEN-LAST:event_botonModificarDireccionActionPerformed
