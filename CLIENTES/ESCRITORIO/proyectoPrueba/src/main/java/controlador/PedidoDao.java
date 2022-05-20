f6e09e82e0ec06ec885941f89e3695a83cdd0140
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-20 18:57:05 +0200

    autoCommit 20220520.185705

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/PedidoDao.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/PedidoDao.java
@@ -18,6 +18,31 @@ public class PedidoDao{
     
     public static ArrayList<Pedido> seleccionPedidosPagados() {
         
+        JsonArray jsonArray = gestion.consultaSeleccion(new Peticion("obtener_pedidos_pagados", null));
+
+        ArrayList<Pedido> listaPedidosPagados = new ArrayList<Pedido>();
+
+        for (int i = 0; i < jsonArray.size(); i++) {
+            Pedido pedido = new Pedido();
+            
+            JsonObject pedidoJson = jsonArray.get(i).getAsJsonObject();
+            
+            pedido.setId_pedido(pedidoJson.get("id").getAsInt());
+            pedido.setId_usuario(pedidoJson.get("id_usuario").getAsInt());
+            //pedido.setFecha_hora(pedidoJson.get("fecha_hora").getAsString());
+            pedido.setEstado(pedidoJson.get("estado").getAsString());
+            pedido.setTotal(pedidoJson.get("total").getAsDouble());
+            pedido.setTransporte(pedidoJson.get("transporte").getAsString());
+            
+            
+            listaPedidosPagados.add(pedido);
+
+        }
+        return listaPedidosPagados;
+    }
+    
+    public static ArrayList<Pedido> seleccionPedidosEnPreparacion() {
+        
         JsonArray jsonArray = gestion.consultaSeleccion(new Peticion("obtener_usuarios", null));
 
         ArrayList<Pedido> listaPedidosPagados = new ArrayList<Pedido>();
