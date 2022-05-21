cc354dc13211c9728acb2bddf941ef059a838691
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-21 17:00:54 +0200

    autoCommit 20220521.170054

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/DialogoImagen.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/DialogoImagen.java
@@ -74,11 +74,10 @@ public class DialogoImagen extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                JFileChooser jf = new JFileChooser();
-            }
-        });
+     
+            JFileChooser jf = new JFileChooser();
+            jf.setDialogTitle("Seleccionar Imagen");
+       
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
