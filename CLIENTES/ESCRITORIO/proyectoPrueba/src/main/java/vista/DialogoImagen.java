656644d71a51ffe657aec8258b7cca502c07b31c
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-21 16:58:45 +0200

    autoCommit 20220521.165845

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/DialogoImagen.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/DialogoImagen.java
@@ -94,12 +94,6 @@ public class DialogoImagen extends javax.swing.JDialog {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(DialogoImagen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(DialogoImagen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(DialogoImagen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(DialogoImagen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
