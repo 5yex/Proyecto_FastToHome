976e7c695c0ac57d4e9b4b18515616c91e7da7cc
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-21 19:44:11 +0200

    autoCommit 20220521.194410

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/administrador/VentanaAdmin.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/administrador/VentanaAdmin.java
@@ -181,7 +181,8 @@ public class VentanaAdmin extends javax.swing.JFrame {
 
     private void itemMercaderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemMercaderActionPerformed
         //hacerMecader
-        UsuarioDao.asignarRolMercader()
+        Usuario nuevoMercader = 
+        UsuarioDao.asignarRolMercader(new Usuario(tablaClientes.getSelectedRow()));
     }//GEN-LAST:event_itemMercaderActionPerformed
 
     private void itemAdministradorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemAdministradorActionPerformed
