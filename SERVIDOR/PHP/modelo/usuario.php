34e785f181dee13607af5462ea613afe2b99e232
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-21 19:25:53 +0200

    autoCommit 20220521.192553

--- SERVIDOR/PHP/modelo/usuario.php
+++ SERVIDOR/PHP/modelo/usuario.php
@@ -226,7 +226,7 @@ class usuario extends conexion {
         $sentencia->bindParam(":email", $email);
         $sentencia->bindParam(":tlf", $tlf);
         $sentencia->bindParam(":rol", $rol);
-        $sentencia->bindParam(":id", $id);
+        $sentencia->bindParam(":id_user", $id);
         $sentencia->bindParam(":pass", $password);
 
         // $sentencia->bindParam(":fot", $this->getFoto() );
