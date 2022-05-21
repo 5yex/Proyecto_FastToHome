e4e3528dcc758ed79a7de9fd6892ef4e5c58030b
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-21 19:48:58 +0200

    autoCommit 20220521.194858

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/DialogoImagen.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/DialogoImagen.java
@@ -128,7 +128,7 @@ public class DialogoImagen extends javax.swing.JDialog {
             }   Imagen img = new Imagen();
            
        
-            byte[] imageBytes = IOUtils.toByteArray(new FileImageInputStream(imagenSeleccionada));
+            byte[] imageBytes = IOUtils.toByteArray(new FileInputStream(imagenSeleccionada));
             String base64 = Base64.getEncoder().encodeToString(imageBytes);
                         System.out.println(base64);
 
