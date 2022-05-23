ab11e7a12d9308344780e6cc380d92193791ca3d
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-23 19:52:14 +0200

    autoCommit 20220523.195214

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/ImagenDao.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/ImagenDao.java
@@ -4,10 +4,15 @@
  */
 package controlador;
 
+import modelo.Categoria;
+import modelo.Peticion;
+
 /**
  *
  * @author jmcbg
  */
 public class ImagenDao {
-    
+     public static boolean nuevoUsuario(Categoria categoria) {
+        return gestion.hacerConsulta(new Peticion("nueva_imagen", categoria.getJSON()));
+    }
 }
