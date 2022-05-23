7880ba0e54f5f24503a5ba9e4fad00c5d35ec4bc
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-23 19:28:06 +0200

    autoCommit 20220523.192806

--- SERVIDOR/PHP/webService/prueba.php
+++ SERVIDOR/PHP/webService/prueba.php
@@ -1,9 +1,16 @@
 
 <?php
-$imgData = "iVBORw0KGgoAAAANSUhEUgAAAAYAAAAFCAIAAADpOgqxAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAjSURBVBhXY/iPAUBCb2VUICSEAVUF5wMBVBWEj6IKAf7/BwD490snvc/CaAAAAABJRU5ErkJggg==";
+//$imgData = "iVBORw0KGgoAAAANSUhEUgAAAAYAAAAFCAIAAADpOgqxAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAjSURBVBhXY/iPAUBCb2VUICSEAVUF5wMBVBWEj6IKAf7/BwD490snvc/CaAAAAABJRU5ErkJggg==";
+//$img = "<img src= 'data:image;base64, $imgData' />";
+//print($img);
+
+//
+//file_put_contents( "imagenes/prueba.png", base64_decode($imgData));
+
+
+$imgData = base64_encode(file_get_contents( "imagenes/prueba.png"));
+
+
 $img = "<img src= 'data:image;base64, $imgData' />";
+
 print($img);
-
-
-file_put_contents( "imagenes/prueba.png", base64_decode($imgData));
-
