436d2c05ac2e9831c0b22361a61d0c6713180304
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-23 19:22:44 +0200

    autoCommit 20220523.192244

--- SERVIDOR/PHP/modelo/Imagen.php
+++ SERVIDOR/PHP/modelo/Imagen.php
@@ -64,7 +64,7 @@ class Imagen extends Conexion{
     }
     
     public function obtenerImagenPorId(){
-        $sql = "SELECT * FROM imegenes WHERE id = :id";
+        $sql = "SELECT * FROM imagenes WHERE id = :id";
         
         $sentencia = $this->dblink->prepare($sql);
 
