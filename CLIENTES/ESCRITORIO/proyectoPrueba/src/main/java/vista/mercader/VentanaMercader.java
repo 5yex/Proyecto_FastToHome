deae680d9be7cf86fdc8dcb8297a24fe54898d59
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-24 22:34:16 +0200

    autoCommit 20220524.223416

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/VentanaMercader.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/VentanaMercader.java
@@ -232,7 +232,6 @@ public class VentanaMercader extends javax.swing.JFrame {
     }
 
     private void mostrarPedidos() {
-        crearHiloActualizacionDeProductos();
         ActionListener actualizarPedidos = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
