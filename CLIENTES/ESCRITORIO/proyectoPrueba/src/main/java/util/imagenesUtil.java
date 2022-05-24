d60f11f4f2541a51d3b6e7121d0762a1121c67d2
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-24 23:25:26 +0200

    autoCommit 20220524.232526

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/util/imagenesUtil.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/util/imagenesUtil.java
@@ -7,10 +7,13 @@ package util;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Base64;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.ImageIcon;
 import org.apache.commons.io.IOUtils;
 
 /**
@@ -18,7 +21,8 @@ import org.apache.commons.io.IOUtils;
  * @author jmcbg
  */
 public class imagenesUtil {
-    public static String imagenABase64(File imagenFile){
+
+    public static String imagenABase64(File imagenFile) {
         try {
             byte[] imageBytes = IOUtils.toByteArray(new FileInputStream(imagenFile));
             return Base64.getEncoder().encodeToString(imageBytes);
@@ -29,19 +33,10 @@ public class imagenesUtil {
         }
         return null;
     }
-    
-    
-    public static String base64AImagen(File imagenFile){
-        try {
-            byte[] imageBytes = IOUtils.toByteArray(new FileInputStream(imagenFile));
-            return Base64.getEncoder().encodeToString(imageBytes);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(imagenesUtil.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(imagenesUtil.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
+
+    public ImageIcon base64AImagen(String imagenBase64) {
+        byte[] decodedBytes = Base64.getDecoder().decode(imagenBase64);
+        return new ImageIcon(decodedBytes);
     }
-    
-    
+
 }
