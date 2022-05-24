4160cbfbb3b0e6c646eadffb71f5714599775850
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-24 22:53:56 +0200

    autoCommit 20220524.225356

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/VentanaMercader.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/VentanaMercader.java
@@ -253,6 +253,7 @@ public class VentanaMercader extends javax.swing.JFrame {
         for (Pedido pedido : pedidos) {
             contenido.add(new panelPedido(pedido, actualizarPedidos));
         }
+        contenido.revalidate();
         contenido.repaint();
 
     }
