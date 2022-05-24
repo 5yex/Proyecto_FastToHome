dd1e1519849e08c095696d45bae3a2a2cefd6b05
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-25 00:12:08 +0200

    autoCommit 20220525. 01208

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/PedidoDao.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/PedidoDao.java
@@ -19,97 +19,94 @@ import modelo.Peticion;
  *
  * @author Jesus
  */
-public class PedidoDao{
-    
+public class PedidoDao {
+
     public static ArrayList<Pedido> seleccionPedidosPagados() {
-        
+
         JsonArray jsonArray = gestion.consultaSeleccion(new Peticion("obtener_pedidos_pagados", null));
 
         ArrayList<Pedido> listaPedidosPagados = new ArrayList<Pedido>();
 
         for (int i = 0; i < jsonArray.size(); i++) {
             Pedido pedido = new Pedido();
-            
+
             JsonObject pedidoJson = jsonArray.get(i).getAsJsonObject();
-            
+
             pedido.setId_pedido(pedidoJson.get("id").getAsInt());
             pedido.setId_usuario(pedidoJson.get("id_usuario").getAsInt());
             //pedido.setFecha_hora(pedidoJson.get("fecha_hora").getAsString());
             pedido.setEstado(pedidoJson.get("estado").getAsString());
             pedido.setTotal(pedidoJson.get("total").getAsDouble());
             pedido.setTransporte(pedidoJson.get("transporte").getAsString());
-            
-            
+
             listaPedidosPagados.add(pedido);
 
         }
         return listaPedidosPagados;
     }
-    
+
     public static ArrayList<Pedido> seleccionPedidosEnPreparacion() {
-        
+
         JsonArray jsonArray = gestion.consultaSeleccion(new Peticion("obtener_pedidos_en_preparacion", null));
 
         ArrayList<Pedido> listaPedidosEnPreparacion = new ArrayList<Pedido>();
 
         for (int i = 0; i < jsonArray.size(); i++) {
             Pedido pedido = new Pedido();
-            
+
             JsonObject pedidoJson = jsonArray.get(i).getAsJsonObject();
-            
+
             pedido.setId_pedido(pedidoJson.get("id").getAsInt());
             pedido.setId_usuario(pedidoJson.get("id_usuario").getAsInt());
             //pedido.setFecha_hora(pedidoJson.get("fecha_hora").getAsString());
             pedido.setEstado(pedidoJson.get("estado").getAsString());
             pedido.setTotal(pedidoJson.get("total").getAsDouble());
             pedido.setTransporte(pedidoJson.get("transporte").getAsString());
-            
-            
+
             listaPedidosEnPreparacion.add(pedido);
 
         }
         return listaPedidosEnPreparacion;
     }
-    
+
     public static ArrayList<Pedido> seleccionTodosPedidos(Negocio negocio) {
         SimpleDateFormat dateParser = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
-        
+
         JsonArray jsonArray = gestion.consultaSeleccion(new Peticion("obtener_todos_pedidos", negocio.getJSON()));
 
         ArrayList<Pedido> listaPedidosEnPreparacion = new ArrayList<Pedido>();
 
-        if(listaPedidosEnPreparacion != null){
-        for (int i = 0; i < jsonArray.size(); i++) {
-            Pedido pedido = new Pedido();
-            
-            JsonObject pedidoJson = jsonArray.get(i).getAsJsonObject();
-            
-            pedido.setId_pedido(pedidoJson.get("id").getAsInt());
-            pedido.setId_usuario(pedidoJson.get("id_usuario").getAsInt());
-            String fechaActual = pedidoJson.get("fecha_hora").getAsString();
-            
-            try {
-                pedido.setFecha_hora(dateParser.parse(fechaActual));
-            } catch (ParseException ex) {
-                Logger.getLogger(PedidoDao.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
-            pedido.setEstado(pedidoJson.get("estado").getAsString());
-            pedido.setTotal(pedidoJson.get("total").getAsDouble());
-            pedido.setTransporte(pedidoJson.get("transporte").getAsString());
-            
-            
-            listaPedidosEnPreparacion.add(pedido);
+        if (jsonArray != null) {
+            for (int i = 0; i < jsonArray.size(); i++) {
+                Pedido pedido = new Pedido();
 
-        }
-        return listaPedidosEnPreparacion;
-        }else{
+                JsonObject pedidoJson = jsonArray.get(i).getAsJsonObject();
+
+                pedido.setId_pedido(pedidoJson.get("id").getAsInt());
+                pedido.setId_usuario(pedidoJson.get("id_usuario").getAsInt());
+                String fechaActual = pedidoJson.get("fecha_hora").getAsString();
+
+                try {
+                    pedido.setFecha_hora(dateParser.parse(fechaActual));
+                } catch (ParseException ex) {
+                    Logger.getLogger(PedidoDao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                pedido.setEstado(pedidoJson.get("estado").getAsString());
+                pedido.setTotal(pedidoJson.get("total").getAsDouble());
+                pedido.setTransporte(pedidoJson.get("transporte").getAsString());
+
+                listaPedidosEnPreparacion.add(pedido);
+
+            }
+            return listaPedidosEnPreparacion;
+        } else {
             System.out.println("no hay ningun pedido");
         }
         return null;
     }
-    
-    public static boolean actualizarEstadoPedido(Pedido pedido){
+
+    public static boolean actualizarEstadoPedido(Pedido pedido) {
         return gestion.hacerConsulta(new Peticion("actualizar_estado_pedido", pedido.getJSON()));
     }
 }
