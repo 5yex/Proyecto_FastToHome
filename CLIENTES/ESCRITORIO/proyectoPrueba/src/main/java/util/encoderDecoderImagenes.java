28bead7837068ed92bc9e4f9393e4eb0d51ebbc8
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-24 21:11:47 +0200

    autoCommit 20220524.211147

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/util/encoderDecoderImagenes.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/util/encoderDecoderImagenes.java
@@ -4,10 +4,26 @@
  */
 package util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.IOUtils;
+
 /**
  *
  * @author jmcbg
  */
 public class encoderDecoderImagenes {
-    
+    public static String imagenATextoBase64(File imagenFile){
+        try {
+            byte[] imageBytes = IOUtils.toByteArray(new FileInputStream(imagenFile));
+            return Base64.getEncoder().encodeToString(imageBytes);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(encoderDecoderImagenes.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
 }
