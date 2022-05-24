fd1dca5eb4f77d5a561103146cc8c47355c4be6f
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-24 22:56:58 +0200

    autoCommit 20220524.225658

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/DialogoImagen.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/DialogoImagen.java
@@ -140,10 +140,11 @@ public class DialogoImagen extends javax.swing.JDialog {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         imagen = imagenNueva;
+        this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
 
