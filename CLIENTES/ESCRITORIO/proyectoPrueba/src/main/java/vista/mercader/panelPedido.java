e815ec1f07551a92070f24fa4fd972911d3aa238
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-25 18:29:53 +0200

    autoCommit 20220525.182953

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/panelPedido.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/panelPedido.java
@@ -141,6 +141,7 @@ public class panelPedido extends javax.swing.JPanel {
 
     private void pasarEstadoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pasarEstadoActionPerformed
         if(PedidoDao.actualizarEstadoPedido(pedido)){
+            botonPedidos.doClick();
         }else{
             System.out.println("nO SE PUEDE VISUALIZAR EL NUEVO ESTADO");
         }
