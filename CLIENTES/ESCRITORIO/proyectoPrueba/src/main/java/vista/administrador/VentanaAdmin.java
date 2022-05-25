e47ab381e99016a7b636f4d9b37e30e2cde40aac
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-25 19:15:58 +0200

    autoCommit 20220525.191558

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/administrador/VentanaAdmin.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/administrador/VentanaAdmin.java
@@ -188,8 +188,8 @@ public class VentanaAdmin extends javax.swing.JFrame {
         int filaSeleccionada = tablaClientes.getSelectedRow();
         if (UsuarioDao.asignarRolMercader(new Usuario(arrayListClientes.get(filaSeleccionada).getId()))) {
             recargarTablaClientes(UsuarioDao.obtenerDatosClientes());
-            this.revalidate();
-            this.repaint();
+            //this.revalidate();
+            //this.repaint();
         } else {
             JOptionPane.showMessageDialog(this, "Ha ocurrido un error al asignar como mercader a este usuario", "Error", JOptionPane.ERROR_MESSAGE);
         }
