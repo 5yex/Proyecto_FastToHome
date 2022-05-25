ea99a615626156bd59b1914a45b7a2399614f45f
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-25 21:57:48 +0200

    autoCommit 20220525.215748

--- CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/controlador/gestion.java
+++ CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/controlador/gestion.java
@@ -1,5 +1,21 @@
 package com.donosocortes.fast2home.controlador;
 
+import com.donosocortes.fast2home.modelo.Peticion;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class gestion {
     public static boolean hacerConsulta(Peticion peticion) {
         String json = null;
--- CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/modelo/Imagen.java
+++ CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/modelo/Imagen.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package modelo;
+package com.donosocortes.fast2home.modelo;
 
 import com.google.gson.Gson;
 import java.awt.Image;
