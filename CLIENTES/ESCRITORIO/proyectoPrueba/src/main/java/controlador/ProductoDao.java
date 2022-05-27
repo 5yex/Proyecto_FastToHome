46f70f91c56868a8bfe13c6f9b397f63c8710b66
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-27 21:15:54 +0200

    autoCommit 20220527.211554

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/ProductoDao.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/controlador/ProductoDao.java
@@ -30,7 +30,7 @@ public class ProductoDao {
     public static ArrayList<Producto> selecciónProductosNegocio(Negocio negocio) {
         JsonArray jsonArray = gestion.consultaSeleccion(new Peticion("obtener_productos_negocio", negocio.getJSON()));
         ArrayList<Producto> listaProductos = new ArrayList<>();
-        System.out.println("controlador.ProductoDao.selecciónProductosNegocio()     NUMERO PRODUCTOS: " + listaProductos.size());
+        //System.out.println("controlador.ProductoDao.selecciónProductosNegocio()     NUMERO PRODUCTOS: " + listaProductos.size());
         if (jsonArray != null) {
             for (int i = 0; i < jsonArray.size(); i++) {
                 JsonObject productoJson = jsonArray.get(i).getAsJsonObject();
@@ -45,7 +45,9 @@ public class ProductoDao {
                 }
                 listaProductos.add(producto);
             }
+            System.out.println("controlador.ProductoDao.selecciónProductosNegocio()     NUMERO PRODUCTOS: " + listaProductos.size());
         }
+        
         return listaProductos;
     }
 
