1571cd0b9cfd388aa01719f3d5fae0ec36721764
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-27 19:41:23 +0200

    autoCommit 20220527.194123

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/panelInfoInicial.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/panelInfoInicial.java
@@ -50,6 +50,7 @@ public class panelInfoInicial extends javax.swing.JPanel {
         nombreNegocio = new javax.swing.JLabel();
         DescripcionNegocio = new javax.swing.JLabel();
         img = new javax.swing.JLabel();
+        img1 = new javax.swing.JLabel();
 
         setPreferredSize(new java.awt.Dimension(612, 437));
 
@@ -61,19 +62,28 @@ public class panelInfoInicial extends javax.swing.JPanel {
 
         img.setPreferredSize(new java.awt.Dimension(271, 271));
 
+        img1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/recursos/noIcon.png"))); // NOI18N
+        img1.setPreferredSize(new java.awt.Dimension(271, 271));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addComponent(DescripcionNegocio, javax.swing.GroupLayout.DEFAULT_SIZE, 600, Short.MAX_VALUE)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(img, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(DescripcionNegocio, javax.swing.GroupLayout.DEFAULT_SIZE, 295, Short.MAX_VALUE))
-                    .addComponent(nombreNegocio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(22, 22, 22))
+                        .addComponent(img1, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(22, 22, 22))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(nombreNegocio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -83,8 +93,10 @@ public class panelInfoInicial extends javax.swing.JPanel {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(img, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(DescripcionNegocio, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(98, Short.MAX_VALUE))
+                    .addComponent(img1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(DescripcionNegocio, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(11, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -92,6 +104,7 @@ public class panelInfoInicial extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel DescripcionNegocio;
     private javax.swing.JLabel img;
+    private javax.swing.JLabel img1;
     private javax.swing.JLabel nombreNegocio;
     // End of variables declaration//GEN-END:variables
 
