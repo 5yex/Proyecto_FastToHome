343c2f0ca3f7d5b775c73bc021f9f68b11310f1b
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-28 13:37:51 +0200

    autoCommit 20220528.133751

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/registrarProducto.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/registrarProducto.java
@@ -79,10 +79,9 @@ public class registrarProducto extends javax.swing.JFrame {
         varNombre.setBorder(new FlatButtonBorder());
 
         jScrollPane1.setBorder(null);
+        jScrollPane1.setViewportBorder(new FlatButtonBorder());
 
         textAreaDescripcion.setColumns(20);
-        textAreaDescripcion.setRows(5);
-        textAreaDescripcion.setBorder(new FlatButtonBorder());
         textAreaDescripcion.setMaximumSize(new java.awt.Dimension(188, 84));
         textAreaDescripcion.setMinimumSize(new java.awt.Dimension(188, 84));
         textAreaDescripcion.setPreferredSize(new java.awt.Dimension(188, 84));
