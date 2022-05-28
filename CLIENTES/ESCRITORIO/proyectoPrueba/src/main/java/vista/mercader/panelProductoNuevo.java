692b0b43d5262e77c6fc88c8218e1540fc8d653e
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-28 13:22:18 +0200

    autoCommit 20220528.132218

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/panelProductoNuevo.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/mercader/panelProductoNuevo.java
@@ -61,7 +61,7 @@ public class panelProductoNuevo extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       new registrarProducto(JB)
+       new registrarProducto(actualizar);
     }//GEN-LAST:event_jButton1ActionPerformed
 
 
