e0f67e7ad34e0e30c4ed467b847f5107aa798117
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-28 18:44:36 +0200

    autoCommit 20220528.184436

--- CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/FrameLogin.java
+++ CLIENTES/ESCRITORIO/proyectoPrueba/src/main/java/vista/FrameLogin.java
@@ -47,6 +47,7 @@ public class FrameLogin extends javax.swing.JFrame implements util.Constantes {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Inicio Sesión");
+        setIconImage(getIconImage());
         setResizable(false);
 
         buttonLogin1.setFont(new java.awt.Font("Segoe UI", 3, 18)); // NOI18N
