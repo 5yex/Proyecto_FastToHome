ec4016e4f7234478341276d6cddcbd781c9d1778
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-29 11:50:42 +0200

    autoCommit 20220529.115042

--- CLIENTES/ESCRITORIO/FastToHome/src/FastToHome/principal.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/FastToHome/principal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-package proyectoFastToHome;
+package FastToHome;
 
 import com.formdev.flatlaf.FlatLightLaf;
 import java.io.IOException;
