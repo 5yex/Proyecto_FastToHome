90095afcb557b004a145c67b365029bc846ddd6e
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-29 13:56:52 +0200

    autoCommit 20220529.135652

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/actualizarNegocio.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/actualizarNegocio.java
@@ -35,8 +35,6 @@ public class actualizarNegocio extends javax.swing.JFrame {
         cargaInicial();
     }
 
-    
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
