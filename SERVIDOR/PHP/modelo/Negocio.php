bae28946537912d84c3a4803edf8b26a1e666944
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-29 15:56:29 +0200

    autoCommit 20220529.155629

--- SERVIDOR/PHP/modelo/Negocio.php
+++ SERVIDOR/PHP/modelo/Negocio.php
@@ -136,7 +136,7 @@ class Negocio extends Conexion {
     }
 
     public function modificarNegocio() {
-        $sql = "UPDATE negocio set imagenes_id = :img, Nombre = :nom, Descripcion = :des, id_categoria = :cat  where id = :id_neg";
+        $sql = "UPDATE negocio set imagenes_id = :img, Nombre = :nom, Descripcion = :des, id_categoria = :cat  where id = :25";
 
         $sentencia = $this->dblink->prepare($sql);
         
