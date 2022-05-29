b7d4d919e51add38b7570be69f848ef9fcb9130f
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-29 12:43:13 +0200

    autoCommit 20220529.124313

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/mercader/panelProducto.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/mercader/panelProducto.java
@@ -153,10 +153,6 @@ public class panelProducto extends javax.swing.JPanel {
     }
 
     private void editarProducto() {
-        if (new editarProducto(btUpdate,producto).setVisible(true)) {
-                JOptionPane.showMessageDialog(null, "Se eliminó el producto.", "Producto eliminado", JOptionPane.INFORMATION_MESSAGE);
-            } else {
-                JOptionPane.showMessageDialog(null, "No se pudo eliminar el producto.", "Producto no eliminado", JOptionPane.INFORMATION_MESSAGE);
-        }
+        new editarProducto(btUpdate,producto).setVisible(true);
     }
 }
