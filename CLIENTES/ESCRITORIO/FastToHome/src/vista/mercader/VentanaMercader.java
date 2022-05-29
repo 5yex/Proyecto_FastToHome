a8a1a20923322d9f4d7bf8b66fd5e45ffee5b7c3
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-29 15:44:11 +0200

    autoCommit 20220529.154411

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/mercader/VentanaMercader.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/mercader/VentanaMercader.java
@@ -359,8 +359,6 @@ public class VentanaMercader extends javax.swing.JFrame {
         DialogoActualizarDatos dmod = new DialogoActualizarDatos(this, true, mercader);
         dmod.setVisible(true);
         cargarInicio();
-
-
     }//GEN-LAST:event_botonModificarDatosMercaderActionPerformed
 
     private void checkBusquedaProductosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkBusquedaProductosActionPerformed
