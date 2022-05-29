f42ff76e90ddfd3d96107ee2fb1bceccc2f888ac
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-29 13:47:23 +0200

    autoCommit 20220529.134723

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/FrameLogin.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/FrameLogin.java
@@ -40,7 +40,9 @@ public class FrameLogin extends javax.swing.JFrame implements util.Constantes {
         imagenesUtil.imagenProyectoAjlabel(new ImageIcon(getClass().getResource("/recursos/logoGrandeBlanco.png")), jLabel1);
         this.setIconImage(new ImageIcon(getClass().getResource("/recursos/logoIcon.png")).getImage());
         String[] combo = {" ","Tema Claro", "Tema Oscuro"};
-        System.out.println(UIManager.getLookAndFeel().getName());
+        if(UIManager.getLookAndFeel().getName()== "FlatLaf Dark"){
+            jComboBox1.setSelectedIndex(1);
+        }
     }
 
     @SuppressWarnings("unchecked")
