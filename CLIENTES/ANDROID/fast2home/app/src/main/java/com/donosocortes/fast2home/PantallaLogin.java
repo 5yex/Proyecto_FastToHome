387b11ddbb7dd67203d85c16dc98942a4313feb7
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-30 18:57:43 +0200

    autoCommit 20220530.185743

--- CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaLogin.java
+++ CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaLogin.java
@@ -4,26 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.donosocortes.fast2home.controlador.UsuarioDao;
-import com.donosocortes.fast2home.modelo.Usuario;
-
-import java.util.List;
-
 public class PantallaLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_login);
-        List<Usuario> lista = UsuarioDao.seleccionUsuarios();
-
-        Runnable runnable = new Runnable(){
-            public void run() {
-                System.out.println(lista.size());
-            }
-        };
-        runnable.run();
-
     }
 
 
