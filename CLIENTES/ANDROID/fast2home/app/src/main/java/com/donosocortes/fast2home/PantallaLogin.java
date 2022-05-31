9e417b577b5832ad6fe00cc6901f7114eeb77005
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-31 19:51:16 +0200

    autoCommit 20220531.195116

--- CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaLogin.java
+++ CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaLogin.java
@@ -46,7 +46,7 @@ public class PantallaLogin extends AppCompatActivity {
 
                 try {
                     if((new JSONObject(response).getBoolean("error")) == true){
-                        new VolleyError();
+                        new VolleyError("Error de login");
                     }
 
                     JSONObject resp = new JSONObject(response).getJSONObject("datos");
