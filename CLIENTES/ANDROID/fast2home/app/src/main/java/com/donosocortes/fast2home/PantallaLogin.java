922d4828d15469e9a024c2b84e5c9488db5d6aa0
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-31 19:55:27 +0200

    Revert "autoCommit 20220531.195514"
    This reverts commit e985dab5ba772b16568d8e3e95afc9917e7d7fca.

--- CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaLogin.java
+++ CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaLogin.java
@@ -9,6 +9,8 @@ import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.donosocortes.fast2home.modelo.Peticion;
@@ -43,7 +45,11 @@ public class PantallaLogin extends AppCompatActivity {
                 System.out.println(response);
 
                 try {
-                    JSONObject resp = new JSONObject(response).getJSONObject("datos");
+                    if((new JSONObject(response).getBoolean("error")) == true){
+                        throw new VolleyError("Error de login");
+                    }
+
+                    JSONObject resp = new JSONObject(response).getJSONArray("datos").getJSONObject(0);
                     user.setPassword(resp.getString("password"));
                     user.setId(resp.getInt("id"));
                     Toast.makeText(PantallaLogin.this, user.toString(), Toast.LENGTH_SHORT).show();
@@ -52,8 +58,11 @@ public class PantallaLogin extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
-        }, error -> {
-            Toast.makeText(PantallaLogin.this, "Fail to get response = " + error, Toast.LENGTH_SHORT).show();
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(PantallaLogin.this, "Fail to get response = " + error, Toast.LENGTH_SHORT).show();
+            };
         }) {
             @Override
             protected Map<String, String> getParams() {
