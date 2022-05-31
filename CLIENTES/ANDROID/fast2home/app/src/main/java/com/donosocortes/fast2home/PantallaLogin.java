347c16e28d9e05622fa2327a003c275b9c06f875
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-31 20:16:50 +0200

    autoCommit 20220531.201650

--- CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaLogin.java
+++ CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaLogin.java
@@ -54,6 +54,8 @@ public class PantallaLogin extends AppCompatActivity {
                         user.setId(datos.getInt("id"));
                         if(BCrypt.checkpw(password.getText().toString(),user.getPassword())){
                             Toast.makeText(PantallaLogin.this, "Has iniciado sesión correctamente", Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(PantallaLogin.this, "Tu contraseña es incorrecta", Toast.LENGTH_SHORT).show();
                         }
                     }
                 } catch (JSONException | VolleyError e) {
