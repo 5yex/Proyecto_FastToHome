06b83b6d725a7aeac9d76aef14317ef6ea9ef4cf
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-05-31 19:59:11 +0200

    autoCommit 20220531.195911

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/FrameLogin.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/FrameLogin.java
@@ -62,7 +62,7 @@ public class FrameLogin extends javax.swing.JFrame implements util.Constantes {
 
         buttonLogin1.setBackground(new java.awt.Color(255, 51, 0));
         buttonLogin1.setFont(new java.awt.Font("Segoe UI", 3, 18)); // NOI18N
-        buttonLogin1.setForeground(new java.awt.Color(204, 204, 204));
+        buttonLogin1.setForeground(new java.awt.Color(255, 255, 255));
         buttonLogin1.setText("ENTRAR");
         buttonLogin1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -74,7 +74,9 @@ public class FrameLogin extends javax.swing.JFrame implements util.Constantes {
 
         emailTxt.setText("CORREO ELECTRÓNICO");
 
+        buttonRegistro.setBackground(new java.awt.Color(255, 51, 0));
         buttonRegistro.setFont(new java.awt.Font("Segoe UI", 3, 18)); // NOI18N
+        buttonRegistro.setForeground(new java.awt.Color(255, 255, 255));
         buttonRegistro.setText("REGISTRO");
         buttonRegistro.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
