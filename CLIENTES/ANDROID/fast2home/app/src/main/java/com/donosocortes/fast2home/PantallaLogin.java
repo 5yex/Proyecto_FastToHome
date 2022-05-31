ee930a8d112246b622b3ae432de54816f04cdfc6
José Miguel <83497667+5yex@users.noreply.github.com>
2022-05-31 19:07:42 +0200

    autoCommit 20220531.190742

--- CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaLogin.java
+++ CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaLogin.java
@@ -3,17 +3,88 @@ package com.donosocortes.fast2home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.donosocortes.fast2home.modelo.Usuario;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PantallaLogin extends AppCompatActivity {
 
+    private EditText email;
+    private EditText password;
+    private Usuario user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_login);
+        email = findViewById(R.id.eTEmail);
+        password = findViewById(R.id.eTPassword);
     }
 
-    public void comprobarPass(){
-        
-    }
+    public void comprobarPass() {
+        // url to post our data
+        String url = "https://reqres.in/api/users";
 
+
+        // creating a new variable for our request queue
+        RequestQueue queue = Volley.newRequestQueue(PantallaLogin.this);
+
+        // on below line we are calling a string
+        // request method to post the data to our API
+        // in this we are calling a post method.
+        StringRequest request = new StringRequest(Request.Method.POST, url, new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // inside on response method we are
+                // hiding our progress bar
+                // and setting data to edit text as empty
+                //loadingPB.setVisibility(View.GONE);
+                // nameEdt.setText("");
+                // jobEdt.setText("");
+
+                // on below line we are displaying a success toast message.
+                // Toast.makeText(MainActivity.this, "Data added to API", Toast.LENGTH_SHORT).show();
+                //try {
+                // on below line we are parsing the response
+                // to json object to extract data from it.
+                // JSONObject respObj = new JSONObject(response);
+
+                // below are the strings which we
+                // extract from our json object.
+                //  String name = respObj.getString("name");
+                // String job = respObj.getString("job");
+
+                // on below line we are setting this string s to our text view.
+                // responseTV.setText("Name : " + name + "\n" + "Job : " + job);
+                //} catch (JSONException e) {
+                //e.printStackTrace();
+                //}
+            }
+        }, error -> {
+            // method to handle errors.
+            Toast.makeText(PantallaLogin.this, "Fail to get response = " + error, Toast.LENGTH_SHORT).show();
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                user = new Usuario();
+                user.setEmail(email.getText().toString());
+                //  params.put("job", job);
+                return params;
+            }
+        };
+        // below line is to make
+        // a json object request.
+        queue.add(request);
+    }
 }
+
+
