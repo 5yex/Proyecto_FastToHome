9d29117710b34869d5c97051f2fbdf9e45b4947a
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-01 19:36:03 +0200

    autoCommit 20220601.193603

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/FrameLogin.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/FrameLogin.java
@@ -203,6 +203,7 @@ public class FrameLogin extends javax.swing.JFrame implements util.Constantes {
                             JOptionPane.showMessageDialog(this, "No está disponible una aplicación de escritorio para los clientes.\n"
                                     + "Si quieres registrar tu negocio y acceder como mercader, solicítalo a los adminsitradores.\n"
                                     + "Si eres un nuevo administrador y no puedes entrar, espera a que un administrador te registre como tal.", "Advertencia.", JOptionPane.INFORMATION_MESSAGE);
+                            return false;
                             break;
                         case ADMIN:
                             this.setEnabled(false);
