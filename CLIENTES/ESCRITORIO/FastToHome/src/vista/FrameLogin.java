e9a1ea391582cc5fc3f9ed39397ca2d964dc7b40
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-02 18:34:48 +0200

    autoCommit 20220602.183448

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/FrameLogin.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/FrameLogin.java
@@ -160,8 +160,7 @@ public class FrameLogin extends javax.swing.JFrame implements util.Constantes {
     private void buttonLogin1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonLogin1ActionPerformed
         if(comprobarLogin()){
             this.dispose();
-        }
-        
+        }      
     }//GEN-LAST:event_buttonLogin1ActionPerformed
 
     private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
