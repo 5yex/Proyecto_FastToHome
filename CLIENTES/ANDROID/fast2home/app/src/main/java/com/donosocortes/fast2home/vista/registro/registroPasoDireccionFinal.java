bc01dfb4232276a952989977d9e485f569a20e9b
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-03 18:37:10 +0200

    autoCommit 20220603.183710

---
+++ CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/vista/registro/registroPasoDireccionFinal.java
@@ -0,0 +1,14 @@
+package com.donosocortes.fast2home.vista.registro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registroPasoDireccionFinal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_paso_direccion_final);
+    }
+}
