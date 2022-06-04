95ccc94349373266cf50bf1dc6a94808c94ebae8
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-04 17:18:58 +0200

    autoCommit 20220604.171857

--- CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/ui/notifications/NotificationsFragment.java
+++ CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/ui/notifications/NotificationsFragment.java
@@ -10,6 +10,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.donosocortes.fast2home.databinding.FragmentNotificationsBinding;
+
 
 public class NotificationsFragment extends Fragment {
 
