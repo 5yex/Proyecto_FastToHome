704fc81c11feaeb1087c4041052089a749a37267
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-04 19:32:44 +0200

    autoCommit 20220604.193244

--- CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaInicio.java
+++ CLIENTES/ANDROID/fast2home/app/src/main/java/com/donosocortes/fast2home/PantallaInicio.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import com.donosocortes.fast2home.vista.login.PantallaLogin;
 
@@ -14,6 +15,8 @@ public class PantallaInicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
     }
 
     public void pantallaLogin(View view){
