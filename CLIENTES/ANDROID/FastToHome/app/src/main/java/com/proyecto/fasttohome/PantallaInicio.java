abd71eb85cd907124fdbc4b4fd1baa9c78aed872
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-05 12:28:50 +0200

    autoCommit 20220605.122850

--- CLIENTES/ANDROID/FastToHome/app/src/main/java/com/proyecto/fasttohome/PantallaInicio.java
+++ CLIENTES/ANDROID/FastToHome/app/src/main/java/com/proyecto/fasttohome/PantallaInicio.java
@@ -14,7 +14,7 @@ public class PantallaInicio extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      
+
         setContentView(R.layout.activity_main);
 
     }
