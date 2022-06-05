91ed5d28178048f67b45558d8a6b502a566814e0
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-05 17:32:17 +0200

    autoCommit 20220605.173217

--- CLIENTES/ANDROID/FastToHome/app/src/main/java/com/proyecto/fasttohome/vista/principal/menu/Pedidos.java
+++ CLIENTES/ANDROID/FastToHome/app/src/main/java/com/proyecto/fasttohome/vista/principal/menu/Pedidos.java
@@ -52,7 +52,6 @@ public class Pedidos extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        actionBar.setHomeButtonEnabled(true);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
