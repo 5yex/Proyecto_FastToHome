ef57d6ab2e99742e340a84a2fbde2e5a96a5859d
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-05 19:45:07 +0200

    autoCommit 20220605.194507

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/registroNegocio.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/registroNegocio.java
@@ -200,12 +200,13 @@ public class registroNegocio extends javax.swing.JFrame {
     }//GEN-LAST:event_botonPedirImagenActionPerformed
 
     private void cargaInicial() {
-        textAreaDescripcion.setLineWrap(true);
+        
         listaCategorias = controlador.CategoriaDao.obtenerCategorias();
         for (Categoria listaCategoria : listaCategorias) {
             comboCategoria.addItem(listaCategoria.getNombre());
         }
         JOptionPane.showMessageDialog(null, User.getNombre() + ", bienvenido al equipo de mercaderes! \n A continuación podrás configurar tu negocio.","Bienvenida mercader", JOptionPane.INFORMATION_MESSAGE);
+        textAreaDescripcion.setLineWrap(true);
     }
 
     private boolean nuevoNegocio(Usuario user) {
