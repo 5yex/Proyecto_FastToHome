ed6d7d85f41a966518a19bbfb5179e00b0e75737
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-05 13:28:38 +0200

    autoCommit 20220605.132838

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/administrador/VentanaAdmin.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/administrador/VentanaAdmin.java
@@ -602,7 +602,9 @@ public class VentanaAdmin extends javax.swing.JFrame {
     }
 
     public void confirmarSalida() {
-        int valor = JOptionPane.showConfirmDialog(this, "¿Esta seguro de que quiere abandonar esta ventana?", "Advertencia", JOptionPane.YES_NO_OPTION);
+        /*this, "¿Esta seguro de que quiere abandonar esta ventana?", "Advertencia", 
+        JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, new Object[]{"Si","No"},"Si"*/
+        int valor = JOptionPane.showOptionDialog();
         if (valor == JOptionPane.YES_OPTION) {
 
             JOptionPane.showMessageDialog(null, "Volverá a la ventana de Logueo", "Hasta luego!", JOptionPane.INFORMATION_MESSAGE);
