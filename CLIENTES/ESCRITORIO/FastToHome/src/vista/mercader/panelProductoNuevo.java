565363969e7a89b24a062c6d33beaf7356f2d084
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-06 10:23:18 +0200

    autoCommit 20220606.102318

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/mercader/panelProductoNuevo.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/mercader/panelProductoNuevo.java
@@ -67,7 +67,8 @@ public class panelProductoNuevo extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       new registrarProducto(actualizar,negocio).setVisible(true);
+       //new registrarProducto(actualizar,negocio).setVisible(true);
+       new DialogoRegistrarProducto(this, actualizar, negocio, true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
 
