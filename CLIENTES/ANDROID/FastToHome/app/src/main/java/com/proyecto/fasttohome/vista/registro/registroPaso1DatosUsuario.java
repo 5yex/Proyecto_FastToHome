fab5ec3db23ea0851218d3e25c5d19d682fd88ff
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-06 20:55:50 +0200

    autoCommit 20220606.205549

--- CLIENTES/ANDROID/FastToHome/app/src/main/java/com/proyecto/fasttohome/vista/registro/registroPaso1DatosUsuario.java
+++ CLIENTES/ANDROID/FastToHome/app/src/main/java/com/proyecto/fasttohome/vista/registro/registroPaso1DatosUsuario.java
@@ -27,7 +27,7 @@ public class registroPaso1DatosUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
-       // getSupportActionBar().hide();
+        // getSupportActionBar().hide();
         setContentView(R.layout.activity_registro_paso_datos_usuario);
 
         nombre = findViewById(R.id.etNombre);
@@ -43,31 +43,34 @@ public class registroPaso1DatosUsuario extends AppCompatActivity {
     public void irPasoPassword(View view) {
         //Transporte de los datos
         String name = nombre.getText().toString();
-                String ape = apellidos.getText().toString();
-                String nif = dni.getText().toString();
-                String telefono = telefono.getText().toString();
-                String correo = email.getText().toString();
+        String ape = apellidos.getText().toString();
+        String nif = dni.getText().toString();
+        String numTelefono = telefono.getText().toString();
+        String correo = email.getText().toString();
         user.setNombre(nombre.getText().toString());
         user.setApellidos(apellidos.getText().toString());
         user.setDni(dni.getText().toString());
         user.setTlf(telefono.getText().toString());
         user.setEmail(email.getText().toString());
 
-        Intent i = new Intent(this, registroPaso2Password.class );
+        Intent i = new Intent(this, registroPaso2Password.class);
 
         i.putExtra("user", user);
         i.putExtra("direccion", direccion);
 
         startActivity(i);
-    };
+    }
+
+    ;
 
     private static long back_pressed;
+
     @Override
     public void onBackPressed() {
         if (back_pressed + 2000 > System.currentTimeMillis()) {
             super.onBackPressed();
         } else {
-            Toast.makeText(getBaseContext(), "Presiona otra vez sin quieres salir del registro!",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getBaseContext(), "Presiona otra vez sin quieres salir del registro!", Toast.LENGTH_SHORT).show();
         }
         back_pressed = System.currentTimeMillis();
     }
