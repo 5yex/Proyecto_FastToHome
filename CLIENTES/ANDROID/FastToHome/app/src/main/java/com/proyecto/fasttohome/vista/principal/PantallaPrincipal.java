359a2c5b676a346a9ff8c4587c3d39b44480e86d
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-06 20:51:50 +0200

    autoCommit 20220606.205150

--- CLIENTES/ANDROID/FastToHome/app/src/main/java/com/proyecto/fasttohome/vista/principal/PantallaPrincipal.java
+++ CLIENTES/ANDROID/FastToHome/app/src/main/java/com/proyecto/fasttohome/vista/principal/PantallaPrincipal.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.view.Window;
 
 import com.proyecto.fasttohome.R;
+import com.proyecto.fasttohome.modelo.Usuario;
 
 public class PantallaPrincipal extends AppCompatActivity {
+
+    private Usuario user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +18,12 @@ public class PantallaPrincipal extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_pantalla_principal);
+
+        user = (Usuario) getIntent().getExtras().getSerializable("user");
+        System.out.println("User: " + user.toString());
     }
+
+
 
 
 
