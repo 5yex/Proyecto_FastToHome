fb0d09861855f7b9b62608fa00732e4b2b8d90e0
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-06 20:55:09 +0200

    autoCommit 20220606.205509

--- CLIENTES/ANDROID/FastToHome/app/src/main/java/com/proyecto/fasttohome/vista/principal/PantallaPrincipal.java
+++ CLIENTES/ANDROID/FastToHome/app/src/main/java/com/proyecto/fasttohome/vista/principal/PantallaPrincipal.java
@@ -3,10 +3,26 @@ package com.proyecto.fasttohome.vista.principal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.proyecto.fasttohome.R;
+import com.proyecto.fasttohome.modelo.Peticion;
 import com.proyecto.fasttohome.modelo.Usuario;
+import com.proyecto.fasttohome.vista.login.PantallaLogin;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PantallaPrincipal extends AppCompatActivity {
 
@@ -21,6 +37,36 @@ public class PantallaPrincipal extends AppCompatActivity {
 
         user = (Usuario) getIntent().getExtras().getSerializable("user");
         System.out.println("User: " + user.toString());
+    }
+
+    public void obtenerDatosUsuario() {
+        String url = getString(R.string.apiUrl);
+        RequestQueue queue = Volley.newRequestQueue(PantallaPrincipal.this);
+        StringRequest request = new StringRequest(Request.Method.POST, url, response -> {
+            System.out.println(response);
+            try {
+                JSONObject resp = new JSONObject(response);
+                if ((resp.getBoolean("error")) == true) {
+                    throw new VolleyError(resp.getString("datos"));
+                } else {
+                    JSONObject datos = resp.getJSONArray("datos").getJSONObject(0);
+                    user.setPassword(datos.getString("password"));
+                    user.setId(datos.getInt("id"));
+
+
+                }
+            } catch (JSONException | VolleyError e) {
+                Toast.makeText(PantallaPrincipal.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }, error -> Toast.makeText(PantallaPrincipal.this, "ERROR DE CONEXIÓN = " + error, Toast.LENGTH_SHORT).show()) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("DATA", new Peticion("getHash", user.getJSON()).getJSON());
+                return params;
+            }
+        };
+        queue.add(request);
     }
 
 
