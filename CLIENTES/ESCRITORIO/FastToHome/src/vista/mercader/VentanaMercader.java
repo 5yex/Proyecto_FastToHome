0f56b64257ce5c4b8bf6aac5dc5972e949a48aff
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-06 10:21:32 +0200

    autoCommit 20220606.102132

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/mercader/VentanaMercader.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/mercader/VentanaMercader.java
@@ -438,7 +438,7 @@ public class VentanaMercader extends javax.swing.JFrame {
         //interrumpirHilo();
         panelProductos.removeAll();
         panelProductos.setLayout(new WrapLayout(FlowLayout.CENTER, 30, 30));
-        panelProductos.add(new panelProductoNuevo(recargarProductos, negocio));
+        panelProductos.add(new panelProductoNuevo(this,recargarProductos, negocio));
 
         ArrayList<Producto> productos = ProductoDao.selecciónProductosNegocio(negocio);
         if (productos != null) {
