0c2df52ffd5f02a0d9a12a8b36c9792c83cc5e9e
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-06 14:47:27 +0200

    autoCommit 20220606.144727

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/mercader/DialogoRegistrarProducto.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/mercader/DialogoRegistrarProducto.java
@@ -11,6 +11,7 @@ import controlador.DireccionDao;
 import controlador.ImagenDao;
 import controlador.NegocioDao;
 import controlador.ProductoDao;
+import java.awt.Frame;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -44,7 +45,7 @@ public class DialogoRegistrarProducto extends javax.swing.JDialog {
      *
      * @param user
      */
-    public DialogoRegistrarProducto(JFrame parent, JButton btActualizar, Negocio neg, boolean modal) {
+    public DialogoRegistrarProducto(Frame parent, JButton btActualizar, Negocio neg, boolean modal) {
         super(parent,modal);
         initComponents();
         this.btActualizar = btActualizar;
