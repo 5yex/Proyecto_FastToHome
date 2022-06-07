e2e043b735e172743872407fff77fc4f1986be1b
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-07 18:38:12 +0200

    autoCommit 20220607.183812

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/DialogoImagen.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/DialogoImagen.java
@@ -49,7 +49,7 @@ public class DialogoImagen extends javax.swing.JDialog {
     public DialogoImagen(java.awt.Frame parent, boolean modal, Imagen imagen) {
         super(parent, modal);
         initComponents();
-        this.setIconImage(new ImageIcon(getClass().getResource("/recursos/logoIcon.png")).getImage());
+        establecerIcono();
         this.imagen = imagen;
     }
     
