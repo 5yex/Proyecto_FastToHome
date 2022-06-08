e942df8fcd7579fcde2651a3483f752b232055eb
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-08 23:35:13 +0200

    autoCommit 20220608.233513

--- CLIENTES/ESCRITORIO/FastToHome/src/modelo/Usuario.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/modelo/Usuario.java
@@ -10,97 +10,97 @@ import java.util.Vector;
 
 /**
  * Clase que representa un usuario de la aplicación
- * 
+ *
  * @author Jose Miguel Calderón, Jesús Rueda
  * @version 1.0
  * @since 1.0
  */
 public class Usuario {
-    
+
     /**
      * Número que identifica al usuario en la base de datos
-     * 
+     *
      * @since 1.0
      */
     private int id;
-    
+
     /**
      * Nombre del usuario
-     * 
+     *
      * @since 1.0
      */
     private String nombre;
-    
+
     /**
      * Apellidos del usuario
-     * 
+     *
      * @since 1.0
      */
     private String apellidos;
-    
+
     /**
      * DNI del usuario
-     * 
+     *
      * @since 1.0
      */
     private String dni;
-    
+
     /**
      * Correo o email del usuario
-     * 
+     *
      * @since 1.0
      */
     private String email;
-    
+
     /**
      * Número de teléfono del usuario
-     * 
+     *
      * @since 1.0
      */
     private String tlf;
-    
+
     /**
      * Rol establecido para el usuario
-     * 
+     *
      * @since 1.0
      */
     private String rol;
-    
+
     /**
      * Número que identifica la dirección del usuario
-     * 
+     *
      * @since 1.0
      */
     private int id_direccion;
-    
+
     /**
      * Contraseña del usuario
-     * 
+     *
      * @since 1.0
      */
     private String password;
-    
+
     /**
      * Construye un Usuario con parametos a <code>null</code>
-     * 
+     *
      * @since 1.0
-     * 
+     *
      */
     public Usuario() {
     }
-    
+
     /**
      * Construye un Usuario con los parametros indicados
-     * 
+     *
      * @param id Número que identifica al usuario en la base de datos
      */
     public Usuario(int id) {
         this.id = id;
     }
-    
+
     /**
      * Construye un Usuario con los parametros indicados
-     * 
+     *
      * @param id Número que identifica al usuario en la base de datos
      * @param nombre Nombre del usuario
      * @param apellidos Apellidos del usuario
@@ -122,10 +122,10 @@ public class Usuario {
         this.id_direccion = id_direccion;
         this.password = password;
     }
-    
+
     /**
      * Construye un Usuario con los parametros indicados
-     * 
+     *
      * @param nombre Nombre del usuario
      * @param apellidos Apellidos del usuario
      * @param dni DNI del usuario
@@ -145,10 +145,10 @@ public class Usuario {
         this.id_direccion = id_direccion;
         this.password = password;
     }
-    
+
     /**
      * Construye un Usuario con los parametros indicados
-     * 
+     *
      * @param email Correo del usuario
      */
     public Usuario(String email) {
@@ -157,7 +157,7 @@ public class Usuario {
 
     /**
      * Construye un Usuario con los parametros indicados
-     * 
+     *
      * @param email Correo del usuario
      * @param password Contraseña del usuario
      */
@@ -165,149 +165,149 @@ public class Usuario {
         this.email = email;
         this.password = password;
     }
-    
+
     /**
      * Devuelve el número que identifica al usuario en la base de datos
-     * 
+     *
      * @return el número que identifica al usuario en la base de datos
      * @since 1.0
      */
     public int getId() {
         return id;
     }
-    
+
     /**
      * Establece el número que identifica al usuario en la base de datos
-     * 
+     *
      * @param id Número que identifica al usuario en la base de datos
      * @since 1.0
      */
     public void setId(int id) {
         this.id = id;
     }
-    
+
     /**
      * Devuelve el nombre del usuario
-     * 
+     *
      * @return Nombre del usuario
      * @since 1.0
      */
     public String getNombre() {
         return nombre;
     }
-    
+
     /**
      * Establece el nombre del usuario
-     * 
+     *
      * @param nombre Nombre del usuario
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    
+
     /**
      * Devuelve el apellido del usuario
-     * 
+     *
      * @return Apellido del usuario
      * @since 1.0
-     */ 
+     */
     public String getApellidos() {
         return apellidos;
     }
 
     /**
      * Establece el apellido del usuario
-     * 
+     *
      * @param apellidos Apellido del usuario
      * @since 1.0
      */
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
-    
+
     /**
      * Devuelve el DNI del usuario
-     * 
+     *
      * @return DNI del usuario
      * @since 1.0
      */
     public String getDni() {
         return dni;
     }
-    
+
     /**
      * Establece el DNI del usuario
-     * 
+     *
      * @param dni DNI del usuario
      * @since 1.0
      */
     public void setDni(String dni) {
         this.dni = dni;
     }
-    
+
     /**
      * Devuelve el correo del usuario
-     * 
+     *
      * @return Correo del usuario
      * @since 1.0
      */
     public String getEmail() {
         return email;
     }
-    
+
     /**
      * Establece el nombre del usuario
-     * 
+     *
      * @param email Nombre del usuario
      * @since 1.0
      */
     public void setEmail(String email) {
         this.email = email;
     }
-    
+
     /**
      * Devuelve el número de telefono del usuario
-     * 
+     *
      * @return Número de telefono del usuario
      * @since 1.0
      */
     public String getTlf() {
         return tlf;
     }
-    
+
     /**
      * Estable el número de telefono del usuario
-     * 
+     *
      * @param tlf Número de telefono del usuario
      * @since 1.0
      */
     public void setTlf(String tlf) {
         this.tlf = tlf;
     }
-    
+
     /**
      * Devuelve el rol establecido para un usuario
-     * 
+     *
      * @return Rol establecido para un usuario
      * @since 1.0
      */
     public String getRol() {
         return rol;
     }
-    
+
     /**
      * Establece un nuevo rol para el usuario
-     * 
+     *
      * @param rol Rol establecido para un usuario
      * @since 1.0
      */
     public void setRol(String rol) {
         this.rol = rol;
     }
-    
+
     /**
      * Devuelve una cadena con los atributos del objetod de tipo Usuario
-     * 
+     *
      * @return Cadena con los atributos del objetod de tipo Usuario
      * @since 1.0
      */
@@ -315,53 +315,58 @@ public class Usuario {
     public String toString() {
         return "Usuario{" + "id=" + id + ", nombre=" + nombre + ", dni=" + dni + ", email=" + email + ", tlf=" + tlf + ", rol=" + rol + ", id_direccion=" + id_direccion + ", password=" + password + '}';
     }
-    
+
     /**
-     * Devuelve el número que identifica la direccion del usuario en la base de datos
-     * 
-     * @return Número que identifica la direccion del usuario en la base de datos
+     * Devuelve el número que identifica la direccion del usuario en la base de
+     * datos
+     *
+     * @return Número que identifica la direccion del usuario en la base de
+     * datos
      * @since 1.0
      */
     public int getId_direccion() {
         return id_direccion;
     }
-    
+
     /**
-     * Establece un número que identifica la direccion del usuario en la base de datos
-     * 
-     * @param id_direccion Número que identifica la direccion del usuario en la base de datos
+     * Establece un número que identifica la direccion del usuario en la base de
+     * datos
+     *
+     * @param id_direccion Número que identifica la direccion del usuario en la
+     * base de datos
      * @since 1.0
      */
     public void setId_direccion(int id_direccion) {
         this.id_direccion = id_direccion;
     }
-    
+
     /**
      * Devuelve la contraseña del usuario
-     * 
+     *
      * @return Contraseña del usuario
      * @since 1.0
      */
     public String getPassword() {
         return password;
     }
-    
+
     /**
      * Establece la contraseña del usuario
-     * 
+     *
      * @param password Contraseña del usuario
      * @since 1.0
-     * 
+     *
      */
     public void setPassword(String password) {
         this.password = password;
     }
-     public String getJSON(){
-       return new Gson().toJson(this);
+
+    public String getJSON() {
+        return new Gson().toJson(this);
     }
 
     public Object[] getRow() {
-                return new Object[]{id, dni, email, nombre, tlf};
+        return new Object[]{id, dni, email, nombre, tlf};
 
     }
 
