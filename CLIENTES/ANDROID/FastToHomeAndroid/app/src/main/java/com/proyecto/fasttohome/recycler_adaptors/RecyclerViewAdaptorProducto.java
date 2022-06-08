4161f33f70fed4e3c6013b5b896d89b6cbb1e395
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-08 20:56:41 +0200

    autoCommit 20220608.205641

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/recycler_adaptors/RecyclerViewAdaptorProducto.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/recycler_adaptors/RecyclerViewAdaptorProducto.java
@@ -1,10 +1,16 @@
 package com.proyecto.fasttohome.recycler_adaptors;
 
+import android.widget.TextView;
+
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RecyclerViewAdaptorProducto extends RecyclerView.Adapter<RecyclerViewAdaptorProducto.ViewHolder> {
 
-    public static RecyclerViewAdaptorNegocio.ViewHolder extends RecyclerView.ViewHolder{
-        
+    public static ViewHolder extends RecyclerView.ViewHolder{
+        private TextView nombre, descripcion, precio;
+
+        public ViewHolder(View itemView){
+
+        }
     }
 }
