95d6bb2b0df03477e7a9feb70de75a0b27b9455e
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-08 21:10:50 +0200

    autoCommit 20220608.211049

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/PantallaProductos.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/PantallaProductos.java
@@ -5,9 +5,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.proyecto.fasttohome.recycler_adaptors.RecyclerViewAdaptorProducto;
+
 public class PantallaProductos extends AppCompatActivity {
 
-    private RecyclerView recyclerViewProducto
+    private RecyclerView recyclerViewProducto;
+    private RecyclerViewAdaptorProducto adaptorProducto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
