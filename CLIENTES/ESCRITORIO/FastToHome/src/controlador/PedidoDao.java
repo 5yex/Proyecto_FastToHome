af19329e3bbc922e468d27c7716544daf82b48c1
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-08 23:56:06 +0200

    autoCommit 20220608.235606

--- CLIENTES/ESCRITORIO/FastToHome/src/controlador/PedidoDao.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/controlador/PedidoDao.java
@@ -120,7 +120,7 @@ public class PedidoDao {
 
             }
         } else {
-            System.out.println("no hay ningun pedido");
+            //System.out.println("no hay ningun pedido");
         }
         return listaPedidosEnPreparacion;
     }
