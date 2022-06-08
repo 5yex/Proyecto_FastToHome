0513e7f04f47a1384b1bd54d360399628667957c
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-08 23:56:37 +0200

    autoCommit 20220608.235637

--- CLIENTES/ESCRITORIO/FastToHome/src/controlador/gestion.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/controlador/gestion.java
@@ -37,7 +37,7 @@ public class gestion {
 
             List<NameValuePair> params = new ArrayList<>();
 
-            System.out.println("controlador.gestion.hacerConsulta() ---- PETICION ENVIADO: " + peticion.getJSON());
+            //System.out.println("controlador.gestion.hacerConsulta() ---- PETICION ENVIADO: " + peticion.getJSON());
 
             params.add(new BasicNameValuePair("DATA", peticion.getJSON()));
 
@@ -52,7 +52,7 @@ public class gestion {
 
                 String htmlTxt = EntityUtils.toString(entity);
 
-                System.out.println("controlador.gestion.hacerConsulta() ---- RECEPCIÓN: " + htmlTxt);
+                //System.out.println("controlador.gestion.hacerConsulta() ---- RECEPCIÓN: " + htmlTxt);
 
                 JsonObject jsonObject = new JsonParser().parse(htmlTxt).getAsJsonObject();
                 
