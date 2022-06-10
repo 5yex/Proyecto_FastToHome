3c71b95813bd57c54ed981eac4b25e58864c6083
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-10 09:59:41 +0200

    autoCommit 20220610. 95941

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/util/ImagenesUtil.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/util/ImagenesUtil.java
@@ -1,5 +1,12 @@
 package com.proyecto.fasttohome.util;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 public class ImagenesUtil {
-    
+    public Bitmap BaseStringToBitmap(String imageBase64)
+    {
+        byte[] imageBytes = Convert.FromBase64String(imageBase64);
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.Length);
+    }
 }
