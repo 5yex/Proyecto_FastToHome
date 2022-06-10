2badda49896456adf8a7f13f53502729dbc78015
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-10 18:33:01 +0200

    autoCommit 20220610.183301

--- CLIENTES/ESCRITORIO/FastToHome/src/modelo/Pedido.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/modelo/Pedido.java
@@ -261,15 +261,6 @@ public class Pedido {
     public String getJSON(){
        return new Gson().toJson(this);
     }
-    
-    /**
-     * Devuelve un array de objetos con la información para rellenar la fila de una tabla con información del pedido
-     * 
-     * @return Array de objetos con la información para rellenar la fila de una tabla con información del pedido
-     */
-    public Object[] getRow() {
-        return new Object[]{id_pedido, id_usuario, transporte, total, fecha_hora, estado};
-    }
 
     @Override
     public String toString() {
