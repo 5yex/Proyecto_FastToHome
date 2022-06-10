312319414e894f97e0433aee77fa11614db8417e
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-10 10:05:50 +0200

    autoCommit 20220610.100550

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/PantallaDeNegocios.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/PantallaDeNegocios.java
@@ -65,7 +65,7 @@ public class PantallaDeNegocios extends AppCompatActivity {
                         negocio.setId_negocio(Integer.parseInt(objetoNegocioJSon.get("id").toString()));
                         negocio.setNombre(objetoNegocioJSon.get("Nombre").toString());
                         negocio.setDescripcion(objetoNegocioJSon.get("Descripcion").toString());
-                        negocio.setId_img(objetoNegocioJSon.getString(""));
+                        negocio.setId_img(objetoNegocioJSon.getInt(""));
                         negocios.add(negocio);
                     }
 
