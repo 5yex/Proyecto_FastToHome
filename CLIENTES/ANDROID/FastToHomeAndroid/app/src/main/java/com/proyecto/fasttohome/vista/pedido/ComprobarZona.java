1ad1968911625a73219859977e878bb5f3f469de
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-12 01:42:32 +0200

    autoCommit 20220612. 14232

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/ComprobarZona.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/ComprobarZona.java
@@ -10,7 +10,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.proyecto.fasttohome.vista.pedido.databinding.ActivityComprobarZonaBinding;
+import com.proyecto.fasttohome.R;
+import com.proyecto.fasttohome.databinding.ActivityComprobarZonaBinding;
 
 public class ComprobarZona extends FragmentActivity implements OnMapReadyCallback {
 
