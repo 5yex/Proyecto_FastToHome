2b6b934d551527179372309532638bc8668446c9
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-12 01:33:15 +0200

    autoCommit 20220612. 13315

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/FinalizarPedido.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/FinalizarPedido.java
@@ -101,7 +101,7 @@ public class FinalizarPedido extends AppCompatActivity {
             lista.add(leftPad("€"+producto.getPrecio(),5)+leftPad("  Uds: "+ entry.getValue(),12)+"  "+producto.getNombre());
             indice++;
         }
-        total.setText(String.valueOf(precioTotal + "Euros"));
+        total.setText(String.valueOf(precioTotal + " Euros"));
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,lista);
         listaProductos = findViewById(R.id.list);
         listaProductos.setAdapter(adapter);
