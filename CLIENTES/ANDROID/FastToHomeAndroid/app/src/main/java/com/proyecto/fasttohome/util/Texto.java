e7ae613a32248a21287652b577bf8e82a561f658
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-12 00:39:10 +0200

    autoCommit 20220612. 03910

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/util/Texto.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/util/Texto.java
@@ -1,2 +1,13 @@
-package com.proyecto.fasttohome.util;public class Texto {
+package com.proyecto.fasttohome.util;
+
+public class Texto {
+
+    private static String genString(int len) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
+
 }
