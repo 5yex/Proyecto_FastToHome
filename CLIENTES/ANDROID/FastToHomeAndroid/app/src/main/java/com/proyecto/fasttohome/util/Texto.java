c64c4ecba130620b112382fd43f8b3f80d6d53fd
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-12 00:41:45 +0200

    autoCommit 20220612. 04145

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/util/Texto.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/util/Texto.java
@@ -2,14 +2,14 @@ package com.proyecto.fasttohome.util;
 
 public class Texto {
 
-    private static String genString(int len) {
+    public static String genString(int len) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < len; i++) {
             sb.append(' ');
         }
         return sb.toString();
     }
-    private static String leftPad(String in, int len) {
+    public static String leftPad(String in, int len) {
         return new StringBuilder(in) //
                 .append(genString(len - in.length() - 1)).toString();
     }
