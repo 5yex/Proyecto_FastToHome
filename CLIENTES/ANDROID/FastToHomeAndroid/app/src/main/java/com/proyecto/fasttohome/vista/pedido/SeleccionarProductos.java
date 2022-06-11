12c6584a3b072fc4059696bf07d8090368cdc78a
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-11 20:00:32 +0200

    autoCommit 20220611.200032

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/SeleccionarProductos.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/SeleccionarProductos.java
@@ -89,7 +89,9 @@ public class SeleccionarProductos extends AppCompatActivity {
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
+    
+
+    public boolean aBoolean(MotionEvent event) {
         double precioTotal = 0;
         for(Map.Entry<Integer, Integer> entry : productosSeleccionados.entrySet()) {
             precioTotal = precioTotal + (productos.get(entry.getKey()).getPrecio() * entry.getValue());
