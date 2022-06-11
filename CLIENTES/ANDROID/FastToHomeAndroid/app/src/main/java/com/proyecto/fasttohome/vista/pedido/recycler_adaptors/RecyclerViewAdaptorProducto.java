19bcfab69d03b876c2efad1d9da054a913fcfdfb
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-11 20:36:06 +0200

    autoCommit 20220611.203606

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/recycler_adaptors/RecyclerViewAdaptorProducto.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/recycler_adaptors/RecyclerViewAdaptorProducto.java
@@ -115,13 +115,11 @@ public class RecyclerViewAdaptorProducto extends RecyclerView.Adapter<RecyclerVi
         for(Map.Entry<Integer, Integer> entry : productosSeleccionados.entrySet()) {
             precioTotal = precioTotal + (productos.get(entry.getKey()).getPrecio() * entry.getValue());
         }
-        
-        if(precioTotal > 0) {
+
+
             System.out.println("FKSDFSD");
             pedir.setText("Productos: " + productosSeleccionados.size() + " Precio: " + precioTotal +"\nHACER PEDIDO");
-        }else {
-            pedir.setText("Pedir");
-        }
+        
 
     }
 
