1b6c888d106b3929cb06e00f6b35ca331d78836a
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-12 00:35:17 +0200

    autoCommit 20220612. 03517

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/FinalizarPedido.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/FinalizarPedido.java
@@ -61,7 +61,6 @@ public class FinalizarPedido extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        listaProductos = findViewById(R.id.list);
         usuario = (Usuario) getIntent().getExtras().getSerializable("user");
         negocio = (Negocio) getIntent().getExtras().getSerializable("negocio");
         productos = (HashMap<Integer, Producto>) getIntent().getExtras().getSerializable("productos");
@@ -95,7 +94,7 @@ public class FinalizarPedido extends AppCompatActivity {
             indice++;
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,lista);
-
+        listaProductos = findViewById(R.id.list);
         listaProductos.setAdapter(adapter);
         centimos = (int)(precioTotal*100);
     }
