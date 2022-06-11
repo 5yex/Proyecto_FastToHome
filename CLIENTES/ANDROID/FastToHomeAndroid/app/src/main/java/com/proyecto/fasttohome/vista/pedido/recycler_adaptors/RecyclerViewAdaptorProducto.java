11d2aabebb18c72618474ad7685f13e3116c0dde
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-11 20:29:24 +0200

    autoCommit 20220611.202924

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/recycler_adaptors/RecyclerViewAdaptorProducto.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/recycler_adaptors/RecyclerViewAdaptorProducto.java
@@ -40,6 +40,7 @@ public class RecyclerViewAdaptorProducto extends RecyclerView.Adapter<RecyclerVi
             eliminar = (Button) itemView.findViewById(R.id.DEL);
             info = (Button) itemView.findViewById(R.id.info);
             image = (ImageView) itemView.findViewById(R.id.imageProducto);
+            pedir = (Button) itemView.findViewById(R.id.doPedido);
             }
     }
 
@@ -121,9 +122,9 @@ public class RecyclerViewAdaptorProducto extends RecyclerView.Adapter<RecyclerVi
         for(Map.Entry<Integer, Integer> entry : productosSeleccionados.entrySet()) {
             precioTotal = precioTotal + (productos.get(entry.getKey()).getPrecio() * entry.getValue());
         }
-        binding.doPedido.setText("Productos: " + productosSeleccionados.size() + " Precio: " + precioTotal +"\nHACER PEDIDO");
-        return super.dispatchTouchEvent(event);
 
+        Button pedido =
+        pedido.setText("Productos: " + productosSeleccionados.size() + " Precio: " + precioTotal +"\nHACER PEDIDO");
     }
 
 
