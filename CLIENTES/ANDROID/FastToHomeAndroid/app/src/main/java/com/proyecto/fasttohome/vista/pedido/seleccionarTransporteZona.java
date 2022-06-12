63be950f96ce8871bcc3db2a63d0be9b2e2aee63
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-12 04:46:49 +0200

    autoCommit 20220612. 44649

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/seleccionarTransporteZona.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/seleccionarTransporteZona.java
@@ -2,17 +2,22 @@ package com.proyecto.fasttohome.vista.pedido;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
 import com.proyecto.fasttohome.R;
 
 public class seleccionarTransporteZona extends AppCompatActivity {
     MapView mapView;
     GoogleMap map;
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,13 +27,13 @@ public class seleccionarTransporteZona extends AppCompatActivity {
         mapView.onCreate(savedInstanceState);
 
         // Gets to GoogleMap from the MapView and does initialization stuff
-        map = mapView.getMap();
+        map = mapView.g
         map.getUiSettings().setMyLocationButtonEnabled(false);
         map.setMyLocationEnabled(true);
 
         // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
         try {
-            MapsInitializer.initialize(this.getActivity());
+            MapsInitializer.initialize(seleccionarTransporteZona.this);
         } catch (GooglePlayServicesNotAvailableException e) {
             e.printStackTrace();
         }
