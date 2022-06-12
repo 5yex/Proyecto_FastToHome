95020a68a473090878950bd38ec9d59087033db5
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-12 19:05:22 +0200

    autoCommit 20220612.190522

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/mercader/PanelPedido.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/mercader/PanelPedido.java
@@ -206,7 +206,11 @@ public class PanelPedido extends javax.swing.JPanel {
         Usuario usuario_pedido = UsuarioDao.obtenerUsuarioPorSuId(new Usuario(pedido.getId_usuario()));
         Direccion direccion = DireccionDao.obtenerDireccionUsuario(usuario_pedido);
         
-        String urlDireccion = "https://www.google.es/maps/@"+direccion.getCoordenadas()+",20z";
+       
+       // String urlDireccion = "https://www.google.es/maps/@"+direccion.getCoordenadas()+",20z";
+        
+          String urlDireccion = " http://maps.google.com/maps?q="+direccion.getCoordenadas()+"&ll="+direccion.getCoordenadas()+"&z=20z";
+
             try { 
                 Desktop.getDesktop().browse(new URL(urlDireccion).toURI()); 
             } catch (IOException | URISyntaxException e) { 
