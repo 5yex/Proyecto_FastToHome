7b4c62c872a172949bbf0ad2a37a222d8fb4dbec
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-12 19:46:10 +0200

    autoCommit 20220612.194610

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/DialogoDireccion.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/DialogoDireccion.java
@@ -249,6 +249,7 @@ public class DialogoDireccion extends javax.swing.JDialog implements Constantes{
             String coordenadas = tfCoordenadas.getText();
             
             if(coordenadas.length() > 0 && !validaciones.validar(coordenadas, PATRON_COORDENADAS)){
+                throw new IOException("Ejempo de patron : 32.2424,-4.23234");
             }
             
             if (calle.length() == 0 || ciudad.length() == 0 || num <= 0 || (cp <= 0 && cp >= 99999)) {
