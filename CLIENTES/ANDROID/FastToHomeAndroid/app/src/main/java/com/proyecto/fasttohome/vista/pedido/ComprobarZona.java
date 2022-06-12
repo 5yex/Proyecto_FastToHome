85d256e2330862417b0ab649909024e4dc0f944e
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-12 03:47:42 +0200

    autoCommit 20220612. 34742

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/ComprobarZona.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/ComprobarZona.java
@@ -51,7 +51,7 @@ public class ComprobarZona extends FragmentActivity implements OnMapReadyCallbac
         mMap = googleMap;
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").draggable(false));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 2.0F));
     }
 
