8519e1d25364e01a0f066299c9a4e759e3d7db78
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-12 03:42:14 +0200

    autoCommit 20220612. 34214

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/ComprobarZona.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/ComprobarZona.java
@@ -12,8 +12,12 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -51,21 +55,29 @@ public class ComprobarZona extends FragmentActivity implements OnMapReadyCallbac
     }
 
     private void getLastLocation() {
-        FusedLocationProviderClient fusedLocationClient;
-        fusedLocationClient =
-                LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)
-                == PERMISSION_GRANTED ||
-                ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION)
-                        == PERMISSION_GRANTED) {
-            fusedLocationClient.getCurrentLocation()
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            //(location);
-                        }
-                    });
-        }
+        LocationRequest mLocationRequest = LocationRequest.create();
+        mLocationRequest.setInterval(60000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationCallback mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                Toast.makeText(getApplicationContext()," location result is  " + locationResult, Toast.LENGTH_LONG).show();
+
+                if (locationResult == null) {
+                    Toast.makeText(getApplicationContext(),"current location is null ", Toast.LENGTH_LONG).show();
+
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    if (location != null) {
+                        Toast.makeText(getApplicationContext(),"current location is " + location.getLongitude(), Toast.LENGTH_LONG).show();
+
+                        //TODO: UI updates.
+                    }
+                }
+            }
+        };
     }
 
 
