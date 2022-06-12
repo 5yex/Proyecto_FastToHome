870c9c50c2c01381463f1d320cdfa21d75ac4a76
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-12 20:22:47 +0200

    autoCommit 20220612.202247

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/seleccionarTransporteZona.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/seleccionarTransporteZona.java
@@ -144,13 +144,11 @@ public class seleccionarTransporteZona extends AppCompatActivity implements OnMa
         System.out.println("comenzar pedido");
         pedido = new Pedido();
         pedido.setEstado("pendiente_pago");
-        
         if (view.getId() == R.id.btTransporteDron) {
             pedido.setTransporte("dron");
         } else {
             pedido.setTransporte("repartidor");
         }
-
         Intent i = new Intent(this, PantallaDeNegocios.class);
         i.putExtra("user", user);
         i.putExtra("pedido", pedido);
