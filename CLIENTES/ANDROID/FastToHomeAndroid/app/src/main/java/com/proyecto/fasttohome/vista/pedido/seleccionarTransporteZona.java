06792f63e36a7e310bbae10c5024beb63ac534e1
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-12 04:44:59 +0200

    autoCommit 20220612. 44459

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/seleccionarTransporteZona.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/seleccionarTransporteZona.java
@@ -4,13 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.proyecto.fasttohome.R;
 
 public class seleccionarTransporteZona extends AppCompatActivity {
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccionar_transporte_zona);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        map = mapView.getMap();
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        map.setMyLocationEnabled(true);
+
+        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+        try {
+            MapsInitializer.initialize(this.getActivity());
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+
+        // Updates the location and zoom of the MapView
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
+        map.animateCamera(cameraUpdate);
+
     }
 }
