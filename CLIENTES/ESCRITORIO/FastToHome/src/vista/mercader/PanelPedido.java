9bdde641d847f4a40016f5749160b24c96d7036e
JesusRT97 <79227756+JesusRT97@users.noreply.github.com>
2022-06-12 16:21:14 +0200

    autoCommit 20220612.162114

--- CLIENTES/ESCRITORIO/FastToHome/src/vista/mercader/PanelPedido.java
+++ CLIENTES/ESCRITORIO/FastToHome/src/vista/mercader/PanelPedido.java
@@ -285,7 +285,7 @@ public class PanelPedido extends javax.swing.JPanel {
     
     public void cargarContenidoDeCesta(){
         
-        ArrayList<Cesta> cesta = new ArrayList<Cesta>();
+        ArrayList<Cesta> cesta;
         cesta = CestaDao.obtenerContenidoCestaPedido(pedido);
         DefaultListModel listaModeloCesta = new DefaultListModel();
         System.out.println(cesta.toString());
