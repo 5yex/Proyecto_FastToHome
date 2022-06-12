61d764b906ab5ad459c2132d9c475c648f8ef3b0
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-12 04:38:42 +0200

    autoCommit 20220612. 43842

---
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/seleccionarTransporteZona.java
@@ -0,0 +1,14 @@
+package com.proyecto.fasttohome.vista.pedido;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class seleccionarTransporteZona extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccionar_transporte_zona);
+    }
+}
