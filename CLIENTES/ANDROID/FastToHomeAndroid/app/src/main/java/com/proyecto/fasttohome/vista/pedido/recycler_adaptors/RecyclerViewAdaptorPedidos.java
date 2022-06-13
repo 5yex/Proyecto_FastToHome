f12c4f038e5e9c8f4535203c42320616b90c2fbb
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-13 10:49:46 +0200

    autoCommit 20220613.104946

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/recycler_adaptors/RecyclerViewAdaptorPedidos.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/recycler_adaptors/RecyclerViewAdaptorPedidos.java
@@ -62,7 +62,7 @@ public class RecyclerViewAdaptorPedidos extends RecyclerView.Adapter<RecyclerVie
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_negocio, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_pedido, parent, false);
         ViewHolder viewHolder = new ViewHolder(view);
 
         return viewHolder;
