201981ce7fe31a4ed31786fa63624b6acdbfca58
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-13 16:58:45 +0200

    autoCommit 20220613.165845

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/registro/registroPaso1DatosUsuario.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/registro/registroPaso1DatosUsuario.java
@@ -9,10 +9,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.proyecto.fasttohome.R;
+import com.proyecto.fasttohome.modelo.Peticion;
 import com.proyecto.fasttohome.util.Validaciones;
 import com.proyecto.fasttohome.modelo.Direccion;
 import com.proyecto.fasttohome.modelo.Usuario;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class registroPaso1DatosUsuario extends AppCompatActivity {
@@ -42,7 +54,7 @@ public class registroPaso1DatosUsuario extends AppCompatActivity {
         user = (Usuario) getIntent().getExtras().getSerializable("user");
         funcion = getIntent().getExtras().getString("funcion");
 
-        if(funcion == "update"){
+        if (funcion == "update") {
             titulo.setText("Actualizar Datos:");
         }
 
@@ -60,24 +72,29 @@ public class registroPaso1DatosUsuario extends AppCompatActivity {
 
         try {
 
-            if(name.length() > 0 && ape.length() > 0 && nif.length() > 0 && numTelefono.length() > 0 && correo.length() > 0){
+            if (name.length() > 0 && ape.length() > 0 && nif.length() > 0 && numTelefono.length() > 0 && correo.length() > 0) {
                 int numero_movil = Integer.parseInt(numTelefono);
-                if(validaciones(name,ape,nif,numTelefono,correo)){
-                    Intent i = new Intent(this, registroPaso2Password.class);
-                    i.putExtra("funcion","registro");
-                    i.putExtra("user", user);
-                    i.putExtra("direccion", direccion);
+                if (validaciones(name, ape, nif, numTelefono, correo)) {
 
+                    if (funcion == "update") {
 
-                    startActivity(i);
+                    }
+                    if (funcion == "registro") {
+                        Intent i = new Intent(this, registroPaso2Password.class);
+                        i.putExtra("funcion", "registro");
+                        i.putExtra("user", user);
+                        i.putExtra("direccion", direccion);
+                        startActivity(i);
+                    }
                 }
-            }else{
-                Toast notificacion = Toast.makeText(this,"No puedes dejar ningún campo vacío.",Toast.LENGTH_SHORT);
+            } else {
+                Toast notificacion = Toast.makeText(this, "No puedes dejar ningún campo vacío.", Toast.LENGTH_SHORT);
                 notificacion.show();
 
             }
-        }catch (NumberFormatException ex){
-            Toast notificacion = Toast.makeText(this,"El número de teléfono debe ser un entero de 9 cifras que empiece por 6, 7 o 9.",Toast.LENGTH_SHORT);
+        } catch (
+                NumberFormatException ex) {
+            Toast notificacion = Toast.makeText(this, "El número de teléfono debe ser un entero de 9 cifras que empiece por 6, 7 o 9.", Toast.LENGTH_SHORT);
             notificacion.show();
         }
     }
@@ -96,45 +113,74 @@ public class registroPaso1DatosUsuario extends AppCompatActivity {
         back_pressed = System.currentTimeMillis();
     }
 
-    public boolean validaciones(String name, String ape, String nif, String numTelefono, String correo){
-        if(Validaciones.validar(name,getString(R.string.patron_nombre))) {
+    public boolean validaciones(String name, String ape, String nif, String numTelefono, String correo) {
+        if (Validaciones.validar(name, getString(R.string.patron_nombre))) {
             user.setNombre(name);
-        }else{
-            Toast notificacion = Toast.makeText(this,"Nombre que empiece por mayuscula y resto minúsculas",Toast.LENGTH_SHORT);
+        } else {
+            Toast notificacion = Toast.makeText(this, "Nombre que empiece por mayuscula y resto minúsculas", Toast.LENGTH_SHORT);
             notificacion.show();
             return false;
         }
 
-        if(Validaciones.validar(ape,getString(R.string.patron_apellido))) {
+        if (Validaciones.validar(ape, getString(R.string.patron_apellido))) {
             user.setApellidos(ape);
-        }else{
-            Toast notificacion = Toast.makeText(this,"Apellido que empiece por mayúscula y resto minúsculas, también acepta algunas prenombre como 'del' ",Toast.LENGTH_LONG);
+        } else {
+            Toast notificacion = Toast.makeText(this, "Apellido que empiece por mayúscula y resto minúsculas, también acepta algunas prenombre como 'del' ", Toast.LENGTH_LONG);
             notificacion.show();
             return false;
         }
 
-        if(Validaciones.validar(nif,getString(R.string.patron_dni))){
+        if (Validaciones.validar(nif, getString(R.string.patron_dni))) {
             user.setDni(nif);
-        }else{
+        } else {
             //Toast
             return false;
         }
 
-        if(Validaciones.validar(numTelefono,getString(R.string.patron_telefono))){
+        if (Validaciones.validar(numTelefono, getString(R.string.patron_telefono))) {
             user.setTlf(numTelefono);
-        }else{
-            Toast notificacion = Toast.makeText(this,"DNI inválido",Toast.LENGTH_SHORT);
+        } else {
+            Toast notificacion = Toast.makeText(this, "DNI inválido", Toast.LENGTH_SHORT);
             notificacion.show();
             return false;
         }
 
-        if(Validaciones.validar(correo, getString(R.string.patron_email))){
+        if (Validaciones.validar(correo, getString(R.string.patron_email))) {
             user.setEmail(correo);
-        }else {
-            Toast notificacion = Toast.makeText(this,"Correo inválido. Con '@' y con terminación tras '.' (.es, .com, .org)",Toast.LENGTH_LONG);
+        } else {
+            Toast notificacion = Toast.makeText(this, "Correo inválido. Con '@' y con terminación tras '.' (.es, .com, .org)", Toast.LENGTH_LONG);
             notificacion.show();
             return false;
         }
         return true;
     }
+
+    private void actualizarDatos() {
+        //Obtenemos la url del archivo strings.xml
+        String url = getString(R.string.apiUrl);
+        RequestQueue queue = Volley.newRequestQueue(registroPaso1DatosUsuario.this);
+        StringRequest request = new StringRequest(Request.Method.POST, url, response -> {
+            System.out.println(response);
+            try {
+                JSONObject resp = new JSONObject(response);
+                if ((resp.getBoolean("error")) == true) {
+                    throw new VolleyError(resp.getString("datos"));
+                } else {
+                    Toast.makeText(registroPaso1DatosUsuario.this, "Se ha actualizado con éxito", Toast.LENGTH_SHORT).show();
+
+                }
+            } catch (JSONException | VolleyError e) {
+                Toast.makeText(registroPaso1DatosUsuario.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }, error -> Toast.makeText(registroPaso1DatosUsuario.this, "ERROR DE CONEXIÓN = " + error, Toast.LENGTH_SHORT).show()) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("DATA", new Peticion("nuevo_usuario", user.getJSON()).getJSON());
+                return params;
+            }
+        };
+        queue.add(request);
+    }
+
 }
