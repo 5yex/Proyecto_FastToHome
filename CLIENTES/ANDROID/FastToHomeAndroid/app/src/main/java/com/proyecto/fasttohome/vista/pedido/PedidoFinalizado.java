2cb0dbccf7678b82d0f8ef36cf7cf57045b5d9d0
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-13 09:43:20 +0200

    autoCommit 20220613. 94320

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/PedidoFinalizado.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/PedidoFinalizado.java
@@ -3,6 +3,7 @@ package com.proyecto.fasttohome.vista.pedido;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 import com.proyecto.fasttohome.R;
 
@@ -11,6 +12,8 @@ public class PedidoFinalizado extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_pedido_finalizado);
     }
 }
