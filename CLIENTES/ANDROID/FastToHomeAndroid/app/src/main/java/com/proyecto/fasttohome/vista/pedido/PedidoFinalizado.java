f4620be3b3cac3f1678374c984b4768a660680ed
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-13 09:47:50 +0200

    autoCommit 20220613. 94750

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/PedidoFinalizado.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/PedidoFinalizado.java
@@ -2,12 +2,19 @@ package com.proyecto.fasttohome.vista.pedido;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
 
 import com.proyecto.fasttohome.R;
+import com.proyecto.fasttohome.modelo.Usuario;
+import com.proyecto.fasttohome.vista.PantallaPrincipal;
 
 public class PedidoFinalizado extends AppCompatActivity {
+
+    Button volver;
+    Usuario user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +22,11 @@ public class PedidoFinalizado extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_pedido_finalizado);
+        volver = findViewById(R.id.volver);
+        volver.setOnClickListener(view -> {
+            Intent i = new Intent(this, PantallaPrincipal.class);
+            i.putExtra("user", user);
+            startActivity(i);
+        });
     }
 }
