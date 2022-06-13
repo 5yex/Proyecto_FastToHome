595b425e6efdf59dd2c51de5c151f98908555352
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-13 17:04:20 +0200

    autoCommit 20220613.170420

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/registro/registroPaso1DatosUsuario.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/registro/registroPaso1DatosUsuario.java
@@ -54,7 +54,7 @@ public class registroPaso1DatosUsuario extends AppCompatActivity {
         titulo = findViewById(R.id.titleDatosUsuarios);
         user = (Usuario) getIntent().getExtras().getSerializable("user");
         funcion = getIntent().getExtras().getString("funcion");
-
+        System.out.println(funcion+" "+user.getJSON());
         if (funcion == "update") {
             titulo.setText("Actualizar Datos:");
             nombre.setText(user.getNombre());
