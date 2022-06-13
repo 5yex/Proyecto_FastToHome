e82d06661bb111331c3f98138157715605764587
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-13 17:21:26 +0200

    autoCommit 20220613.172126

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/registro/registroPaso1DatosUsuario.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/registro/registroPaso1DatosUsuario.java
@@ -112,12 +112,14 @@ public class registroPaso1DatosUsuario extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (back_pressed + 2000 > System.currentTimeMillis()) {
-            super.onBackPressed();
-        } else {
-            Toast.makeText(getBaseContext(), "Presiona otra vez sin quieres salir del registro!", Toast.LENGTH_SHORT).show();
-        }
-        back_pressed = System.currentTimeMillis();
+        if(funcion.equals("registro")){
+            if (back_pressed + 2000 > System.currentTimeMillis()) {
+                super.onBackPressed();
+            } else {
+                Toast.makeText(getBaseContext(), "!Presiona de nuevo quieres salir del registro!", Toast.LENGTH_SHORT).show();
+            }
+            back_pressed = System.currentTimeMillis();
+        };
     }
 
     public boolean validaciones(String name, String ape, String nif, String numTelefono, String correo) {
