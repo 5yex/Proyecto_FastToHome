aa59cc3d9479796b391bdd0c4a3cb375244c985d
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-13 08:45:32 +0200

    autoCommit 20220613. 84532

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/PedidoFinalizado.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/pedido/PedidoFinalizado.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.proyecto.fasttohome.R;
+
 public class PedidoFinalizado extends AppCompatActivity {
 
     @Override
