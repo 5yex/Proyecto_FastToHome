98869f652e8fe28a33a5e308bf881757c862e932
José Miguel <83497667+5yex@users.noreply.github.com>
2022-06-13 17:21:43 +0200

    autoCommit 20220613.172143

--- CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/registro/registroPaso1DatosUsuario.java
+++ CLIENTES/ANDROID/FastToHomeAndroid/app/src/main/java/com/proyecto/fasttohome/vista/registro/registroPaso1DatosUsuario.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,6 +32,7 @@ import java.util.Map;
 public class registroPaso1DatosUsuario extends AppCompatActivity {
     Usuario user = new Usuario();
     Direccion direccion = new Direccion();
+    Button boton;
     EditText nombre;
     EditText apellidos;
     EditText dni;
